10a03cc8f48fdf1bc04c4d6ef865b4bc630d058c
bostonsinaga <bostonsinga@gmail.com>
2024-06-20 22:53:38 +0700

    Fix loop max iteration

--- c++/utils/printer.h
+++ c++/utils/printer.h
@@ -13,7 +13,7 @@ namespace util {
     static void log(std::vector<int> &nums, int step, bool style) {
       std::cout << "Step: " << step << std::endl;
 
-      for (int i = 0; i < 5; i++) {
+      for (int i = 0; i < nums.size(); i++) {
 
         if (style) std::cout << nums[i] << ',';
         else {
