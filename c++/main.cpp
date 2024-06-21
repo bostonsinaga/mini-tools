1482300589819a7db6313afa724d8582e628bbfe
bostonsinaga <bostonsinga@gmail.com>
2024-06-21 23:41:01 +0700

    Set duration as float

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,20 +1,36 @@
 
-#include <ctime>
+#include <chrono>
 
 #include "utils/printer.h"
 #include "utils/reader.h"
 #include "algorithms/bubble-sort.h"
+#include "algorithms/quick-sort.h"
 
 int main() {
-  time_t startTime = std::time(0);
-  std::vector<int> numbers = utils::Reader::txtToNumbers();
+  /** Start Time */
 
-  /** Algorithm test */
+  typedef std::chrono::high_resolution_clock Time;
+  typedef std::chrono::milliseconds ms;
+  typedef std::chrono::duration<float> fsec;
+  auto t0 = Time::now();
 
-  // ...
+  /** Algorithm Test */
 
-  // process time
-  std::cout << "\n\nDURATION: " << std::time(0) - startTime;
+  std::vector<int> numbers = util::Reader::txtToNumbers();
+
+  util::Printer::log(numbers, util::Printer::BAR_STYLE, "BEFORE");
+  std::cout << std::endl;
+
+  sorter::Quick::solve(numbers);
+
+  util::Printer::log(numbers, util::Printer::BAR_STYLE, "AFTER");
+
+  /** Process Time */
+
+  auto t1 = Time::now();
+  fsec fs = t1 - t0;
+  ms d = std::chrono::duration_cast<ms>(fs);
+  std::cout << "\nDURATIONS:\n" << fs.count() << "s\n" << d.count() << "ms\n";
 
   return 0;
 }
