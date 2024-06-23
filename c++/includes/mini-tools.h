70c3d3243d361318e9992cfaf8c741399310edbe
bostonsinaga <bostonsinga@gmail.com>
2024-06-23 22:39:33 +0700

    Initial commit

---
+++ c++/includes/mini-tools.h
@@ -0,0 +1,11 @@
+#ifndef __MINI_TOOLS_H__
+#define __MINI_TOOLS_H__
+
+#include "algorithms/algorithms.h"
+#include "utils/utils.h"
+
+namespace mt = mini_tools;
+namespace mt::algorithms = mt_alg;
+namespace mt::utils = mt_uti;
+
+#endif // __MINI_TOOLS_H__
