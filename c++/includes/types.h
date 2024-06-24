8f37721ee3b3a757d654a8a8b46279ca87445f72
bostonsinaga <bostonsinga@gmail.com>
2024-06-24 14:56:13 +0700

    Define 'cosnt <type> &' typedefs
    Move 'CheckType' to the top

--- c++/includes/types.h
+++ c++/includes/types.h
@@ -12,6 +12,29 @@ namespace mini_tools {
   enum SortOrderFlag {DESCENDING, ASCENDING};
   enum SortPriorityFlag {LETTER_LOWER, LETTER_UPPER};
 
+  /** INSPECTOR */
+
+  namespace CheckType {
+
+    template <typename T>
+    constexpr bool isLetter() {
+      return
+        std::is_same<T, char>::value ||
+        std::is_same<T, wchar_t>::value ||
+        std::is_same<T, std::string>::value;
+    }
+
+    template <typename T>
+    constexpr bool isNumber() {
+      if (!isLetter<T>()) {
+        return
+          std::is_integral<T>::value ||
+          std::is_floating_point<T>::value;
+      }
+      return false;
+    }
+  }
+
   /** SHORTENED TYPES */
   
   typedef short int SI;
@@ -99,28 +122,101 @@ namespace mini_tools {
   typedef std::vector<VEC_DBL_P> VEC2_DBL_P;
   typedef std::vector<VEC_LD_P> VEC2_LD_P;
 
-  /** INSPECTOR */
+  /** SHORTENED CONSTANT REFERENCES */
 
-  namespace CheckType {
+  typedef const bool& CR_BOL;
+  typedef const std::string& CR_STR;
+  typedef const char& CR_CHR;
+  typedef const wchar_t& CR_WCH;
+  typedef const int& CR_INT;
+  typedef const SI& CR_SI;
+  typedef const LI& CR_LI;
+  typedef const LLI& CR_LLI;
+  typedef const USI& CR_USI;
+  typedef const UI& CR_UI;
+  typedef const ULI& CR_ULI;
+  typedef const ULLI& CR_ULLI;
+  typedef const size_t& CR_SZ;
+  typedef const time_t& CR_TM;
+  typedef const float& CR_FLT;
+  typedef const double& CR_DBL;
+  typedef const LD& CR_LD;
 
-    template <typename T>
-    constexpr bool isLetter() {
-      return
-        std::is_same<T, char>::value ||
-        std::is_same<T, wchar_t>::value ||
-        std::is_same<T, std::string>::value;
-    }
+  /** SHORTENED CONSTANT REFERENCE VECTORS */
 
-    template <typename T>
-    constexpr bool isNumber() {
-      if (!isLetter<T>()) {
-        return
-          std::is_integral<T>::value ||
-          std::is_floating_point<T>::value;
-      }
-      return false;
-    }
-  }
+  typedef const VEC_BOL& CR_VEC_BOL;
+  typedef const VEC_STR& CR_VEC_STR;
+  typedef const VEC_CHR& CR_VEC_CHR;
+  typedef const VEC_WCH& CR_VEC_WCH;
+  typedef const VEC_INT& CR_VEC_INT;
+  typedef const VEC_SI& CR_VEC_SI;
+  typedef const VEC_LI& CR_VEC_LI;
+  typedef const VEC_LLI& CR_VEC_LLI;
+  typedef const VEC_USI& CR_VEC_USI;
+  typedef const VEC_UI& CR_VEC_UI;
+  typedef const VEC_ULI& CR_VEC_ULI;
+  typedef const VEC_ULLI& CR_VEC_ULLI;
+  typedef const VEC_SZ& CR_VEC_SZ;
+  typedef const VEC_TM& CR_VEC_TM;
+  typedef const VEC_FLT& CR_VEC_FLT;
+  typedef const VEC_DBL& CR_VEC_DBL;
+  typedef const VEC_LD& CR_VEC_LD;
+
+  typedef const VEC2_BOL& CR_VEC2_BOL;
+  typedef const VEC2_STR& CR_VEC2_STR;
+  typedef const VEC2_CHR& CR_VEC2_CHR;
+  typedef const VEC2_WCH& CR_VEC2_WCH;
+  typedef const VEC2_INT& CR_VEC2_INT;
+  typedef const VEC2_SI& CR_VEC2_SI;
+  typedef const VEC2_LI& CR_VEC2_LI;
+  typedef const VEC2_LLI& CR_VEC2_LLI;
+  typedef const VEC2_USI& CR_VEC2_USI;
+  typedef const VEC2_UI& CR_VEC2_UI;
+  typedef const VEC2_ULI& CR_VEC2_ULI;
+  typedef const VEC2_ULLI& CR_VEC2_ULLI;
+  typedef const VEC2_SZ& CR_VEC2_SZ;
+  typedef const VEC2_TM& CR_VEC2_TM;
+  typedef const VEC2_FLT& CR_VEC2_FLT;
+  typedef const VEC2_DBL& CR_VEC2_DBL;
+  typedef const VEC2_LD& CR_VEC2_LD;
+
+  /** SHORTENED CONSTANT REFERENCE POINTER VECTORS */
+
+  typedef const VEC_BOL_P& CR_VEC_BOL_P;
+  typedef const VEC_STR_P& CR_VEC_STR_P;
+  typedef const VEC_CHR_P& CR_VEC_CHR_P;
+  typedef const VEC_WCH_P& CR_VEC_WCH_P;
+  typedef const VEC_INT_P& CR_VEC_INT_P;
+  typedef const VEC_SI_P& CR_VEC_SI_P;
+  typedef const VEC_LI_P& CR_VEC_LI_P;
+  typedef const VEC_LLI_P& CR_VEC_LLI_P;
+  typedef const VEC_USI_P& CR_VEC_USI_P;
+  typedef const VEC_UI_P& CR_VEC_UI_P;
+  typedef const VEC_ULI_P& CR_VEC_ULI_P;
+  typedef const VEC_ULLI_P& CR_VEC_ULLI_P;
+  typedef const VEC_SZ_P& CR_VEC_SZ_P;
+  typedef const VEC_TM_P& CR_VEC_TM_P;
+  typedef const VEC_FLT_P& CR_VEC_FLT_P;
+  typedef const VEC_DBL_P& CR_VEC_DBL_P;
+  typedef const VEC_LD_P& CR_VEC_LD_P;
+
+  typedef const VEC2_BOL_P& CR_VEC2_BOL_P;
+  typedef const VEC2_STR_P& CR_VEC2_STR_P;
+  typedef const VEC2_CHR_P& CR_VEC2_CHR_P;
+  typedef const VEC2_WCH_P& CR_VEC2_WCH_P;
+  typedef const VEC2_INT_P& CR_VEC2_INT_P;
+  typedef const VEC2_SI_P& CR_VEC2_SI_P;
+  typedef const VEC2_LI_P& CR_VEC2_LI_P;
+  typedef const VEC2_LLI_P& CR_VEC2_LLI_P;
+  typedef const VEC2_USI_P& CR_VEC2_USI_P;
+  typedef const VEC2_UI_P& CR_VEC2_UI_P;
+  typedef const VEC2_ULI_P& CR_VEC2_ULI_P;
+  typedef const VEC2_ULLI_P& CR_VEC2_ULLI_P;
+  typedef const VEC2_SZ_P& CR_VEC2_SZ_P;
+  typedef const VEC2_TM_P& CR_VEC2_TM_P;
+  typedef const VEC2_FLT_P& CR_VEC2_FLT_P;
+  typedef const VEC2_DBL_P& CR_VEC2_DBL_P;
+  typedef const VEC2_LD_P& CR_VEC2_LD_P;
 }
 
 #endif // __MINI_TOOLS__TYPES_H__
