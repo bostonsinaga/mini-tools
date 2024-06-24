317f23334ff34fe0622c18082847ef175d142b81
bostonsinaga <bostonsinga@gmail.com>
2024-06-24 14:59:25 +0700

    Include 'types.h'

--- c++/includes/algorithms/sorters/quick-sort.h
+++ c++/includes/algorithms/sorters/quick-sort.h
@@ -1,17 +1,17 @@
 #ifndef __ALGORITHMS__SORTERS__QUICK_SORT_H__
 #define __ALGORITHMS__SORTERS__QUICK_SORT_H__
 
+#include "../../types.h"
+
 namespace mini_tools {
 namespace algorithms {
 namespace sorters {
 
   class Quick {
   private:
-    typedef const int & INT;
-
     // 'left' and 'right' are index of 'messy'
     static int partition(
-      std::vector<int> &messy, INT left, INT right
+      VEC_INT &messy, CR_INT left, CR_INT right
     ) {
       int i = left - 1;
 
@@ -28,17 +28,17 @@ namespace sorters {
 
     // 'left' and 'right' are index of 'messy'
     static void recursion(
-      std::vector<int> &messy, INT left, INT right
+      VEC_INT &messy, CR_INT left, CR_INT right
     ) {
       if (left < right) {
-        INT piv = partition(messy, left, right);
+        CR_INT piv = partition(messy, left, right);
         recursion(messy, left, piv - 1);
         recursion(messy, piv + 1, right);
       }
     }
 
   public:
-    static void solve(std::vector<int> &messy) {
+    static void solve(VEC_INT &messy) {
       recursion(messy, 0, messy.size() - 1);
     }
   };
