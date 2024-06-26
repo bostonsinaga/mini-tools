0aea6fe33a2018e51046bfee507d37f7e8db47dc
bostonsinaga <bostonsinga@gmail.com>
2024-06-26 16:43:25 +0700

    Increase the number of 'cutVector' method

--- c++/includes/utils/vec-tools.cpp
+++ c++/includes/utils/vec-tools.cpp
@@ -1,37 +1,133 @@
 #ifndef __MINI_TOOLS__UTILS__VEC_TOOLS_CPP__
 #define __MINI_TOOLS__UTILS__VEC_TOOLS_CPP__
 
+#include "../algorithms/calculators.h"
+
 namespace mini_tools {
 namespace utils {
 
   template <typename T>
-  bool VecTools<T>::isIndexOfVector(std::vector<T> *vec, int idx) {
+  bool VecTools<T>::isIndexOf(std::vector<T> &vec, int idx) {
     if (idx < vec->size() && idx >= 0) return true;
     return false;
   }
 
   template <typename T>
-  T VecTools<T>::getIndexOfVector(std::vector<T> *vec, int idx, T defaultReturn) {
-    if (VecTools<T>::isIndexOfVector(vec, idx)) return vec->at(idx);
+  T VecTools<T>::getIndexOf(std::vector<T> &vec, int idx, T defaultReturn) {
+    if (VecTools<T>::isIndexOf(vec, idx)) return vec->at(idx);
     return defaultReturn;
   }
 
   template <typename T>
-  T VecTools<T>::cutVector(std::vector<T> *vec, int idx) {
-    T wasted = vec->at(idx);
-    std::vector<T> tail = std::vector<T>(vec->begin() + idx + 1, vec->end());
-    *vec = std::vector<T>(vec->begin(), vec->begin() + idx);
-    vec->insert(vec->end(), tail.begin(), tail.end());
+  void VecTools<T>::cutInterval(
+    std::vector<T> &vec,
+    std::vector<T> &wasted,
+    int begin, int end
+  ) {
+    int sz = vec.size();
+
+    // bad interval
+    if (sz == 0 || 
+      (begin < 0 && end < 0) ||
+      (begin >= sz && end >= sz)
+    ) { return; }
+
+    // inverted interval
+    if (begin > end) {
+      int buff = begin;
+      begin = end;
+      end = buff;
+    }
+
+    // exceeded interval
+    if (begin < 0) begin = 0;
+    if (end >= sz) end = sz - 1;
+
+    // cutting
+    std::vector<T> tail = std::vector<T>(vec.begin() + end + 1, vec.end());
+    wasted = std::vector<T>(vec.begin() + begin, vec.begin() + end + 1);
+    vec = std::vector<T>(vec.begin(), vec.begin() + begin);
+    vec.insert(vec.end(), tail.begin(), tail.end());
+  }
+
+  template <typename T>
+  std::vector<T> VecTools<T>::cutInterval(std::vector<T> &vec, int begin, int end) {
+    std::vector<T> wasted;
+    VecTools<T>::cutInterval(vec, wasted, begin, end);
     return wasted;
   }
 
   template <typename T>
-  std::vector<T> VecTools<T>::cutVector(std::vector<T> *vec, VEC_INT idxs) {
-    std::vector<T> wastedVec;
-    for (auto &i : idxs) { wastedVec.push_back(VecTools<T>::cutVector(vec, i)); }
-    return wastedVec;
+  void VecTools<T>::cutSingle(std::vector<T> &vec, T &wasted, int idx) {
+    if (vec.size() == 0) return;
+
+    if (idx < 0) idx = 0;
+    else if (idx >= vec.size()) idx = vec.size() - 1;
+
+    wasted = vec[idx];
+    std::vector<T> tail = std::vector<T>(vec.begin() + idx + 1, vec.end());
+    vec = std::vector<T>(vec.begin(), vec.begin() + idx);
+    vec.insert(vec.end(), tail.begin(), tail.end());
   }
-  
+
+  template <typename T>
+  T VecTools<T>::cutSingle(std::vector<T> &vec, int idx) {
+    T wasted = vec[idx];
+    VecTools<T>::cutSingle(vec, wasted, idx);
+    return wasted;
+  }
+
+  template <typename T>
+  void VecTools<T>::cutIndexes(
+    std::vector<T> &vec,
+    std::vector<T> &wasted,
+    VEC_INT idxs,
+    bool lockedIndex
+  ) {
+    if (vec.size() == 0) return;
+    int last = idxs.size() - 1;
+
+    // ascending
+    if (algorithms::Calc::isArithmeticSequence(idxs, 1)) {
+      VecTools<T>::cutInterval(vec, wasted, idxs[0], idxs[last]);
+    }
+    // descending
+    else if (algorithms::Calc::isArithmeticSequence(idxs, -1)) {
+      VecTools<T>::cutInterval(vec, wasted, idxs[last], idxs[0]);
+    }
+    // one by one
+    else {
+      wasted = {};
+      bool prevIndex = -1;
+
+      for (int i = 0; i < idxs.size(); i++) {
+
+        if (!lockedIndex || (prevIndex >= 0 && prevIndex != idxs[i])) {
+          wasted.push_back(VecTools<T>::cutSingle(vec, idxs[i]));
+        }
+
+        if (lockedIndex) {
+          prevIndex = idxs[i];
+
+          for (int j = i+1; j < idxs.size(); j++) {
+            if (idxs[i] < idxs[j]) idxs[j]--;
+          }
+        }
+      }
+    }
+  }
+
+  template <typename T>
+  std::vector<T> VecTools<T>::cutIndexes(
+    std::vector<T> &vec,
+    VEC_INT idxs,
+    bool lockedIndex
+  ) {
+    std::vector<T> wasted;
+    VecTools<T>::cutIndexes(vec, wasted, idxs, lockedIndex);
+    return wasted;
+  }
+
   template<>
   VEC_UI VecTools<>::getDifferencesToSize(
     VEC_UI sizes, UI targetSize
@@ -46,14 +142,14 @@ namespace utils {
     }
 
     VEC_UI differences;
-    for (auto &sz : sizes) { differences.push_back(max - sz); }
+    for (auto &sz : sizes) differences.push_back(max - sz);
     
     return differences;
   }
 
   template <typename T>
-  std::vector<T> VecTools<T>::cleanDuplicateInsideVector(
-    std::vector<T> *vec,
+  std::vector<T> VecTools<T>::cleanDuplicateInside(
+    std::vector<T> &vec,
     std::function<bool(T,T)> equalRule
   ) {
     std::vector<T> wastedVec;
@@ -67,10 +163,10 @@ namespace utils {
 
           // same address
           if (isEqual && std::is_pointer<T>::value) {
-            VecTools<T>::cutVector(vec, j);
+            VecTools<T>::cutSingle(vec, j);
           }
           // same value
-          else wastedVec.push_back(VecTools<T>::cutVector(vec, j));
+          else wastedVec.push_back(VecTools<T>::cutSingle(vec, j));
 
           j--;
         }
@@ -82,7 +178,7 @@ namespace utils {
 
   template <typename T>
   std::vector<T> VecTools<T>::cleanDuplicateToMember(
-    std::vector<T> *vec, T mem,
+    std::vector<T> &vec, T mem,
     std::function<bool(T,T)> equalRule
   ) {
     std::vector<T> wastedVec;
@@ -96,10 +192,10 @@ namespace utils {
         if (first) {
           // same address
           if (isEqual && std::is_pointer<T>::value) {
-            VecTools<T>::cutVector(vec, i);
+            VecTools<T>::cutSingle(vec, i);
           }
           // same value
-          else wastedVec.push_back(VecTools<T>::cutVector(vec, i));
+          else wastedVec.push_back(VecTools<T>::cutSingle(vec, i));
         }
         else first = true;
       }
--- c++/includes/utils/vec-tools.h
+++ c++/includes/utils/vec-tools.h
@@ -10,20 +10,45 @@ namespace utils {
   template <class T = int>
   class VecTools {
   public:
-    static bool isIndexOfVector(std::vector<T> *vec, int idx);
-    static T getIndexOfVector(std::vector<T> *vec, int idx, T defaultReturn);
-    static T cutVector(std::vector<T> *vec, int idx);
-    static std::vector<T> cutVector(std::vector<T> *vec, VEC_INT idxs);
+    static bool isIndexOf(std::vector<T> &vec, int idx);
+    static T getIndexOf(std::vector<T> &vec, int idx, T defaultReturn);
     static VEC_UI getDifferencesToSize(VEC_UI sizes, UI targetSize);
 
-    static std::vector<T> cleanDuplicateInsideVector(
-      std::vector<T> *vec,
+    static void cutInterval(
+      std::vector<T> &vec,
+      std::vector<T> &wasted,
+      int begin, int end
+    );
+
+    static std::vector<T> cutInterval(
+      std::vector<T> &vec,
+      int begin, int end
+    );
+
+    static void cutSingle(std::vector<T> &vec, T &wasted, int idx);
+    static T cutSingle(std::vector<T> &vec, int idx);
+
+    static void cutIndexes(
+      std::vector<T> &vec,
+      std::vector<T> &wasted,
+      VEC_INT idxs,
+      bool lockedIndex = false
+    );
+
+    static std::vector<T> cutIndexes(
+      std::vector<T> &vec,
+      VEC_INT idxs,
+      bool lockedIndex = false
+    );
+
+    static std::vector<T> cleanDuplicateInside(
+      std::vector<T> &vec,
       // repeated and compared 'T' parameters
       std::function<bool(T,T)> equalRule = [](T rep, T com)->bool { return false; }
     );
 
     static std::vector<T> cleanDuplicateToMember(
-      std::vector<T> *vec, T mem,
+      std::vector<T> &vec, T mem,
       // repeated and compared 'T' parameters
       std::function<bool(T,T)> equalRule = [](T rep, T com)->bool { return false; }
     );
