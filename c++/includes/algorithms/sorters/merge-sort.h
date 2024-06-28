a33dad577faebef86d89d6814991602c136b82b5
bostonsinaga <bostonsinga@gmail.com>
2024-06-28 09:43:41 +0700

    Create two methods 'mergeSort' and 'merge'. The 'mergeSort' call itself and 'merge'

--- c++/includes/algorithms/sorters/merge-sort.h
+++ c++/includes/algorithms/sorters/merge-sort.h
@@ -1,25 +1,60 @@
-#ifndef __ALGORITHMS__SORTERS__MERGE_SORT_H__
-#define __ALGORITHMS__SORTERS__MERGE_SORT_H__
+#include "../../types.h"
+#include "../../utils/vec-tools.h"
 
 namespace mini_tools {
 namespace algorithms {
 namespace sorters {
 
   class Merge {
-  private:
-    static void merge() {
-
-    }
-
-    static void divide(std::vector<int> &messy) {
-      
-    }
-
   public:
-    static void solve() {
+    static void merge(
+      VEC_INT &messy, int left, int mid, int right
+    ) {
+      int subSize[2] = {mid - left + 1, right - mid};
 
+      VEC_INT subVec[2] = {
+        utils::VecTools<int>::cutInterval(
+          messy, left, left + subSize[0] - 1, true
+        ),
+        utils::VecTools<int>::cutInterval(
+          messy, left + subSize[0], left + mid + subSize[1], true
+        )
+      };
+
+      int subVecDex[2] = {0, 0},
+          mergedVecDex = left;
+
+      while (subVecDex[0] < subSize[0] &&
+        subVecDex[1] < subSize[1]
+      ) {
+        if (subVec[0][subVecDex[0]] <= subVec[1][subVecDex[1]]) {
+          messy[mergedVecDex] = subVec[0][subVecDex[0]];
+          subVecDex[0]++;
+        }
+        else {
+          messy[mergedVecDex] = subVec[1][subVecDex[1]];
+          subVecDex[1]++;
+        }
+        mergedVecDex++;
+      }
+
+      for (int i = 0; i < 2; i++) {
+        while (subVecDex[i] < subSize[i]) {
+          messy[mergedVecDex] = subVec[i][subVecDex[i]];
+          subVecDex[i]++;
+          mergedVecDex++;
+        }
+      }
+    }
+
+    static void mergeSort(VEC_INT &messy, int begin, int end) {
+
+      if (begin >= end) return;
+      int mid = begin + (end - begin) / 2;
+
+      mergeSort(messy, begin, mid);
+      mergeSort(messy, mid + 1, end);
+      merge(messy, begin, mid, end);
     }
   };
 }}}
-
-#endif // __ALGORITHMS__SORTERS__MERGE_SORT_H__
