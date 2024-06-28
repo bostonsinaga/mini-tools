fcef13925fc39b266a42a19076e4c1c946dfd522
bostonsinaga <bostonsinga@gmail.com>
2024-06-28 16:43:44 +0700

    Add 'comb-sort.h' and 'selection-sort.h'

--- c++/includes/algorithms/sorters/sorters.h
+++ c++/includes/algorithms/sorters/sorters.h
@@ -2,7 +2,9 @@
 #define __ALGORITHMS__SORTERS_H__
 
 #include "bubble-sort.h"
+#include "comb-sort.h"
 #include "merge-sort.h"
+#include "selection-sort.h"
 #include "quick-sort.h"
 
 #endif // __ALGORITHMS__SORTERS_H__
