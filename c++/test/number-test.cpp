b242234fb52cb4c5db45781f8b83c27d2d96e3e6
bostonsinaga <bostonsinga@gmail.com>
2024-07-01 02:12:46 +0700

    Add test for 'merge', 'quick', and 'selection' sorts

--- c++/test/number-test.cpp
+++ c++/test/number-test.cpp
@@ -29,10 +29,37 @@ int main() {
   /** Comb Sort */
   timer.reset();
   mt_uti::Printer<int>::logf(
-    mt_alg::sorters::Comb<int>::solve(mt::VEC_INT(numbers)),
+    mt_alg::sorters::Comb<int>::solve(numbers),
     true, false, filenames[2]
   );
   timer.print(true, "Comb Sort:");
+  std::cout << std::endl;
+
+  /** Merge Sort */
+  timer.reset();
+  mt_uti::Printer<int>::logf(
+    mt_alg::sorters::Merge<int>::solve(numbers),
+    true, false, filenames[3]
+  );
+  timer.print(true, "Merge Sort:");
+  std::cout << std::endl;
+
+  /** Quick Sort */
+  timer.reset();
+  mt_uti::Printer<int>::logf(
+    mt_alg::sorters::Quick<int>::solve(numbers),
+    true, false, filenames[4]
+  );
+  timer.print(true, "Quick Sort:");
+  std::cout << std::endl;
+
+  /** Selection Sort */
+  timer.reset();
+  mt_uti::Printer<int>::logf(
+    mt_alg::sorters::Selection<int>::solve(numbers),
+    true, false, filenames[5]
+  );
+  timer.print(true, "Selection Sort:");
   std::cout << std::endl;
 
   return 0;
