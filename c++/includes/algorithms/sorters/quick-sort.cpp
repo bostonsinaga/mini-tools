0e76f91827a635a06f5f97a670dae1555ff469af
bostonsinaga <bostonsinga@gmail.com>
2024-07-02 11:42:31 +0700

    Add 'randomPartition' method. This will set pivot index to random number
    Declare 'partition' as 'lomutoPartition'
    
    Rename 'left' and 'right' parameters to 'low' and 'high'

--- c++/includes/algorithms/sorters/quick-sort.cpp
+++ c++/includes/algorithms/sorters/quick-sort.cpp
@@ -1,43 +1,60 @@
 #ifndef __ALGORITHMS__SORTERS__QUICK_SORT_CPP__
 #define __ALGORITHMS__SORTERS__QUICK_SORT_CPP__
 
+#include <ctime>
+
 namespace mini_tools {
 namespace algorithms {
 namespace sorters {
 
+  /** Lomuto Partitioning */
   template <typename T>
-  int Quick<T>::partition(
+  int Quick<T>::lomutoPartition(
     std::vector<T> *messy,
-    CR_INT left,
-    CR_INT right,
-    bool ascending
+    CR_INT low,
+    CR_INT high,
+    bool &ascending
   ) {
-    int i = left - 1;
+    int i = low - 1;
 
-    for (int j = left; j < right; j++) {
-      if ((ascending && messy->at(j) < messy->at(right)) ||
-        (!ascending && messy->at(j) > messy->at(right))
+    for (int j = low; j < high; j++) {
+      if ((ascending && messy->at(j) < messy->at(high)) ||
+        (!ascending && messy->at(j) > messy->at(high))
       ) {
         i++;
         std::swap(messy->at(i), messy->at(j));
       }
     }
 
-    std::swap(messy->at(i+1), messy->at(right));
+    std::swap(messy->at(i+1), messy->at(high));
     return i+1;
+  }
+
+  template <typename T>
+  int Quick<T>::randomPartition(
+    std::vector<T> *messy,
+    CR_INT low,
+    CR_INT high,
+    bool &ascending
+  ) {
+    std::srand(std::time(NULL));
+    int random = low + std::rand() % (high - low);
+    std::swap(messy->at(random), messy->at(high));
+    return Quick<T>::lomutoPartition(messy, low, high, ascending);
   }
 
   template <typename T>
   void Quick<T>::recursion(
     std::vector<T> *messy,
-    CR_INT left,
-    CR_INT right,
-    bool ascending
+    CR_INT low,
+    CR_INT high,
+    bool &ascending
   ) {
-    if (left < right) {
-      int piv = partition(messy, left, right, ascending);
-      Quick<T>::recursion(messy, left, piv - 1, ascending);
-      Quick<T>::recursion(messy, piv + 1, right, ascending);
+    if (low < high) {
+      int piv = randomPartition(messy, low, high, ascending);
+      // int piv = lomutoPartition(messy, low, high, ascending);
+      Quick<T>::recursion(messy, low, piv - 1, ascending);
+      Quick<T>::recursion(messy, piv + 1, high, ascending);
     }
   }
 
--- c++/includes/algorithms/sorters/quick-sort.h
+++ c++/includes/algorithms/sorters/quick-sort.h
@@ -7,23 +7,30 @@ namespace mini_tools {
 namespace algorithms {
 namespace sorters {
 
+  /** Lomuto Partitioning */
   template <class T>
   class Quick {
   private:
-    // 'left' and 'right' are index of 'messy'
-    static int partition(
+    // 'low' and 'high' are index of 'messy'
+    static int lomutoPartition(
       std::vector<T> *messy,
-      CR_INT left,
-      CR_INT right,
-      bool ascending
+      CR_INT low,
+      CR_INT high,
+      bool &ascending
     );
 
-    // 'left' and 'right' are index of 'messy'
+    static int randomPartition(
+      std::vector<T> *messy,
+      CR_INT low,
+      CR_INT high,
+      bool &ascending
+    );
+
     static void recursion(
       std::vector<T> *messy,
-      CR_INT left,
-      CR_INT right,
-      bool ascending
+      CR_INT low,
+      CR_INT high,
+      bool &ascending
     );
 
   public:
