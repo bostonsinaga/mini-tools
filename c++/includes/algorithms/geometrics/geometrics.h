c0ff28df2d1536bf497a87dc1a1763066e2842e3
bostonsinaga <bostonsinga@gmail.com>
2024-07-06 16:11:37 +0700

    Initial commit

---
+++ c++/includes/algorithms/geometrics/geometrics.h
@@ -0,0 +1,8 @@
+#ifndef __MINI_TOOLS__ALGORITHMS__GEOMETRICS_H__
+#define __MINI_TOOLS__ALGORITHMS__GEOMETRICS_H__
+
+#include "point.h"
+#include "bounding-box.h"
+#include "ray-casting.h"
+
+#endif // __MINI_TOOLS__ALGORITHMS__GEOMETRICS_H__
