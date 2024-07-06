6c9684cfbc2fc899ae784aa02830846c4884f91a
bostonsinaga <bostonsinga@gmail.com>
2024-07-06 16:20:07 +0700

    Include 'cmath'

--- c++/includes/algorithms/geometrics/point.cpp
+++ c++/includes/algorithms/geometrics/point.cpp
@@ -1,6 +1,7 @@
 #ifndef __MINI_TOOLS__ALGORITHMS__GEOMETRICS__POINT_CPP__
 #define __MINI_TOOLS__ALGORITHMS__GEOMETRICS__POINT_CPP__
 
+#include <cmath>
 #include "point.h"
 
 namespace mini_tools {
