f8858f5b325ece1ce30b8e4166a02c4d9c8fac1f
bostonsinaga <bostonsinga@gmail.com>
2024-07-12 14:44:38 +0700

    Add more includes

--- c++/includes/algorithms/geometrics/geometrics.h
+++ c++/includes/algorithms/geometrics/geometrics.h
@@ -1,8 +1,10 @@
 #ifndef __MINI_TOOLS__ALGORITHMS__GEOMETRICS_H__
 #define __MINI_TOOLS__ALGORITHMS__GEOMETRICS_H__
 
+#include "point.h"
 #include "size.h"
-#include "bounding-box.h"
+#include "rect.h"
+#include "polygon.h"
 #include "ray-casting.h"
 
 #endif // __MINI_TOOLS__ALGORITHMS__GEOMETRICS_H__
