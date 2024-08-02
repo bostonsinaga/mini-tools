1c198d79ce87ee5328699bf20822d94298667093
bostonsinaga <bostonsinga@gmail.com>
2024-08-02 22:32:43 +0700

    Include 'str-tools.h'

--- c++/includes/utils/utils.h
+++ c++/includes/utils/utils.h
@@ -1,10 +1,11 @@
 #ifndef __MINI_TOOLS__UTILS_H__
 #define __MINI_TOOLS__UTILS_H__
 
-#include "vec-tools.h"
-#include "reader.h"
 #include "printer.h"
+#include "reader.h"
+#include "str-tools.h"
 #include "timer.h"
+#include "vec-tools.h"
 
 namespace mt_uti = mini_tools::utils;
 
