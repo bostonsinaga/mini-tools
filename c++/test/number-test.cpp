fada71dd0339cfb84208e24b19b65696b7c4e946
bostonsinaga <bostonsinga@gmail.com>
2024-08-08 13:23:11 +0700

    Using sorting test with attached vector

--- c++/test/number-test.cpp
+++ c++/test/number-test.cpp
@@ -4,10 +4,11 @@
 int main() {
   std::filesystem::create_directory("outputs");
 
-  std::string filenames[6] {
+  std::string filenames[7] {
     "../resources/sample.txt",
     "outputs/output-bubble-sort.txt",
     "outputs/output-comb-sort.txt",
+    "outputs/output-insertion-sort.txt",
     "outputs/output-merge-sort.txt",
     "outputs/output-quick-sort.txt",
     "outputs/output-selection-sort.txt"
@@ -15,48 +16,45 @@ int main() {
 
   // read input
   mt::VEC_INT numbers = mt_uti::Scanner<int>::txtToNumbers(filenames[0]);
+  mt::VEC_INT numbers_copy = numbers;
   std::cout << std::endl;
 
   // start time
   mt_uti::Timer timer;
 
   /** Bubble Sort */
-  mt_uti::Printer<int>::logf(
-    mt_alg::sorters::Bubble<int>::solve(numbers),
-    true, false, filenames[1]
-  );
+  mt_alg::sorters::Bubble<int, int>::solve(numbers, numbers_copy);
+  mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[1]);
   timer.print(true, "Bubble Sort:");
   std::cout << std::endl;
 
   /** Comb Sort */
-  mt_uti::Printer<int>::logf(
-    mt_alg::sorters::Comb<int>::solve(numbers),
-    true, false, filenames[2]
-  );
+  mt_alg::sorters::Comb<int, int>::solve(numbers, numbers_copy);
+  mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[2]);
   timer.print(true, "Comb Sort:");
   std::cout << std::endl;
 
+  /** Insertion Sort */
+  mt_alg::sorters::Insertion<int, int>::solve(numbers, numbers_copy);
+  mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[3]);
+  timer.print(true, "Insertion Sort:");
+  std::cout << std::endl;
+
   /** Merge Sort */
-  mt_uti::Printer<int>::logf(
-    mt_alg::sorters::Merge<int>::solve(numbers),
-    true, false, filenames[3]
-  );
+  mt_alg::sorters::Merge<int, int>::solve(numbers, numbers_copy);
+  mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[4]);
   timer.print(true, "Merge Sort:");
   std::cout << std::endl;
 
   /** Quick Sort */
-  mt_uti::Printer<int>::logf(
-    mt_alg::sorters::Quick<int>::solve(numbers),
-    true, false, filenames[4]
-  );
+  mt_alg::sorters::Quick<int, int>::solve(numbers, numbers_copy);
+  mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[5]);
   timer.print(true, "Quick Sort:");
   std::cout << std::endl;
 
   /** Selection Sort */
-  mt_uti::Printer<int>::logf(
-    mt_alg::sorters::Selection<int>::solve(numbers),
-    true, false, filenames[5]
-  );
+  mt_alg::sorters::Selection<int, int>::solve(numbers, numbers_copy);
+  mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[6]);
   timer.print(true, "Selection Sort:");
   std::cout << std::endl;
 
