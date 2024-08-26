6790180ed64c1c7ee959e44a25a41f4f1310ad7f
bostonsinaga <bostonsinga@gmail.com>
2024-08-26 17:19:46 +0700

    Separate file reader from text scanner

--- c++/includes/utils/reader.cpp
+++ c++/includes/utils/reader.cpp
@@ -2,6 +2,7 @@
 #define __MINI_TOOLS__UTILS__READER_CPP__
 
 #include <fstream>
+#include "reader.h"
 
 namespace mini_tools {
 namespace utils {
@@ -13,7 +14,7 @@ namespace utils {
 
   bool Reader::isSeparator(CR_STR text, CR_INT i) {
     if (i >= 0 && i < text.length()) {
-      if (isSeparator(text[i]) || i == text.length() - 1) {
+      if (Reader::isSeparator(text[i]) || i == text.length() - 1) {
         return true;
       }
       return false;
@@ -21,6 +22,7 @@ namespace utils {
     return false;
   }
 
+  /** WARNING. Unable to read '\n' character */
   void Reader::getFile(
     CR_STR name,
     std::string &textHook
@@ -29,172 +31,6 @@ namespace utils {
     std::ifstream reader(name);
     while (std::getline(reader, line)) { textHook += line; }
     reader.close();
-  }
-
-  template <typename T>
-  T Reader::stringToNumber(CR_STR numStr) {
-    if constexpr (
-      std::is_same<T, int>::value ||
-      std::is_same<T, UI>::value ||
-      std::is_same<T, SI>::value ||
-      std::is_same<T, USI>::value
-    ) {
-      return std::stoi(numStr);
-    }
-    else if constexpr (std::is_same<T, float>::value) {
-      return std::stof(numStr);
-    }
-    else if constexpr (std::is_same<T, double>::value) {
-      return std::stod(numStr);
-    }
-    else if constexpr (std::is_same<T, LD>::value) {
-      return std::stold(numStr);
-    }
-    else if constexpr (
-      std::is_same<T, LI>::value ||
-      std::is_same<T, ULI>::value
-    ) {
-      return std::stol(numStr);
-    }
-    else if constexpr (
-      std::is_same<T, LLI>::value ||
-      std::is_same<T, ULLI>::value ||
-      std::is_same<T, size_t>::value ||
-      std::is_same<T, time_t>::value
-    ) {
-      return std::stoll(numStr);
-    }
-  }
-
-  template <typename T>
-  void Reader::parseNumbers(
-    CR_STR text,
-    VEC<T> &vecHook
-  ) {
-    if constexpr (CheckType::isNumber<T>()) {
-      std::string numStr;
-
-      bool separated = false,
-        hasDecimalPoint = false,
-        skip = false;
-
-      for (int i = 0; i < text.length(); i++) {
-
-        if (separated && std::isdigit(text[i])) {
-          separated = false;
-        }
-
-        if (!separated) {
-          bool isDecimalPoint = numStr.length() > 0 && text[i] == '.';
-
-          if (std::isdigit(text[i]) || (
-            !hasDecimalPoint && isDecimalPoint
-          )) {
-            numStr += text[i];
-            if (isDecimalPoint) hasDecimalPoint = true;
-
-            if (i == text.length() - 1) skip = false;
-            else skip = true;
-          }
-          else skip = false;
-
-          if (!skip &&
-            Reader::isSeparator(text, i) &&
-            numStr.length() > 0
-          ) {
-            vecHook.push_back(Reader::stringToNumber<T>(numStr));
-            hasDecimalPoint = false;
-            numStr = "";
-          }
-        }
-      }
-    }
-    else static_assert(0, "template type");
-  }
-
-  template <typename T>
-  void Reader::parseLetters(
-    CR_STR text,
-    VEC<T> &vecHook
-  ) {
-    if constexpr (CheckType::isLetter<T>()) {
-      bool separated = false;
-      std::string chars;
-
-      for (int i = 0; i < text.length(); i++) {
-
-        if (separated && std::isalpha(text[i])) {
-          separated = false;
-        }
-
-        if (!separated) {
-          chars += text[i];
-
-          if (Reader::isSeparator(text, i)) {
-            vecHook.push_back(chars);
-            separated = true;
-            chars = "";
-          }
-        }
-      }
-    }
-    else static_assert(0, "template type");
-  }
-
-  template <typename T>
-  VEC<T> Reader::parseNumbers(CR_STR text) {
-
-    if constexpr (CheckType::isNumber<T>()) {
-      VEC<T> numbers;
-      Reader::parseNumbers<T>(text, numbers);
-      return numbers;
-    }
-    else {
-      static_assert(0, "template type");
-      return {};
-    }
-  }
-
-  template <typename T>
-  VEC<T> Reader::parseLetters(CR_STR text) {
-
-    if constexpr (CheckType::isLetter<T>()) {
-      VEC<T> letters;
-      Reader::parseLetters<T>(text, letters);
-      return letters;
-    }
-    else {
-      static_assert(0, "template type");
-      return {};
-    }
-  }
-
-  template <typename T>
-  VEC<T> Reader::txtToNumbers(CR_STR filename) {
-
-    if constexpr (CheckType::isNumber<T>()) {
-      std::string text;
-      Reader::getFile(filename, text);
-      return Reader::parseNumbers<T>(text);
-    }
-    else {
-      static_assert(0, "template type");
-      return {};
-    }
-  }
-
-  template <typename T>
-  VEC<T> Reader::txtToLetters(CR_STR filename) {
-
-    if constexpr (CheckType::isLetter<T>()) {
-      std::string text;
-      Reader::getFile(filename, text);
-      return Reader::parseLetters<T>(text);
-    }
-    else {
-      static_assert(0, "template type");
-      return {};
-    }
   }
 }}
 
--- c++/includes/utils/reader.h
+++ c++/includes/utils/reader.h
@@ -11,34 +11,11 @@ namespace utils {
     static bool isSeparator(CR_CHR ch);
     static bool isSeparator(CR_STR text, CR_INT i);
 
-    /** WARNING. Unable to read '\n' character */
     static void getFile(
       CR_STR name,
       std::string &textHook
     );
-
-    template <typename T>
-    static T stringToNumber(CR_STR numStr);
-
-    template <typename T>
-    static void parseNumbers(CR_STR text, VEC<T> &vecHook);
-
-    template <typename T>
-    static void parseLetters(CR_STR text, VEC<T> &vecHook);
-
-    template <typename T>
-    static VEC<T> parseNumbers(CR_STR text);
-
-    template <typename T>
-    static VEC<T> parseLetters(CR_STR text);
-
-    template <typename T>
-    static VEC<T> txtToNumbers(CR_STR filename);
-
-    template <typename T>
-    static VEC<T> txtToLetters(CR_STR filename);
   };
 }}
 
-#include "reader.cpp"
 #endif // __MINI_TOOLS__UTILS__READER_H__
