3661a60ba226f279c2d61d527e55d777df2d9774
bostonsinaga <bostonsinga@gmail.com>
2024-10-14 09:28:12 +0700

    Renamed

---
+++ c++/includes/data-structures/data-structures.h
@@ -0,0 +1,6 @@
+#ifndef __MINI_TOOLS__ALGORITHMS__STRUCTURES_H__
+#define __MINI_TOOLS__ALGORITHMS__STRUCTURES_H__
+
+namespace mt_str = mini_tools::structures;
+
+#endif // __MINI_TOOLS__ALGORITHMS__STRUCTURES_H__
