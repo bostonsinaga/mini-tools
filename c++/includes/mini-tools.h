f497006aae9a6e5cc7429df4b1c8688c42576643
bostonsinaga <bostonsinga@gmail.com>
2024-10-15 18:07:55 +0700

    Include 'types.h' and 'data-structures.h'

--- c++/includes/mini-tools.h
+++ c++/includes/mini-tools.h
@@ -1,9 +1,10 @@
 #ifndef __MINI_TOOLS_H__
 #define __MINI_TOOLS_H__
 
-#include "algorithms/algorithms.h"
+#include "types.h"
 #include "utils/utils.h"
-#include "structures/structures.h"
+#include "algorithms/algorithms.h"
+#include "data-structures/data-structures.h"
 
 namespace mt = mini_tools;
 
