e5ad27defe260553232bf386daf537c8b30392a1
bostonsinaga <bostonsinga@gmail.com>
2025-02-23 21:14:40 +0700

    Switch position of the last 2 parameters of 'cut..' methods

--- includes/utils/vec-tools.h
+++ includes/utils/vec-tools.h
@@ -60,30 +60,30 @@ namespace utils {
       VEC<T> &vec,
       VEC<T> &wasted,
       int begin, int end,
-      CR_BOL onlyWasted = false,
-      CR_BOL validatingIndex = false
+      CR_BOL validatingIndex = false,
+      CR_BOL onlyWasted = false
     );
 
     static VEC<T> cutInterval(
       VEC<T> &vec,
       CR_INT begin, CR_INT end,
-      CR_BOL onlyWasted = false,
-      CR_BOL validatingIndex = false
+      CR_BOL validatingIndex = false,
+      CR_BOL onlyWasted = false
     );
 
     static void cutSingle(
       VEC<T> &vec,
       T &wasted,
       int idx,
-      CR_BOL onlyWasted = false,
-      CR_BOL validatingIndex = false
+      CR_BOL validatingIndex = false,
+      CR_BOL onlyWasted = false
     );
 
     static T cutSingle(
       VEC<T> &vec,
       CR_INT idx,
-      CR_BOL onlyWasted = false,
-      CR_BOL validatingIndex = false
+      CR_BOL validatingIndex = false,
+      CR_BOL onlyWasted = false
     );
 
     /**
@@ -97,18 +97,16 @@ namespace utils {
       VEC<T> &wasted,
       VEC_INT idxs,
       CR_BOL lockedIndex = true,
-      CR_BOL onlyWasted = false,
       CR_BOL validatingIndex = false,
-      CR_BOL removingExceededIndexes = true
+      CR_BOL onlyWasted = false
     );
 
     static VEC<T> cutIndexes(
       VEC<T> &vec,
       CR_VEC_INT idxs,
       CR_BOL lockedIndex = true,
-      CR_BOL onlyWasted = false,
       CR_BOL validatingIndex = false,
-      CR_BOL removingExceededIndexes = true
+      CR_BOL onlyWasted = false
     );
 
     /**
