cbb3d5893c0df455fec40dc89526886d9e18951e
bostonsinaga <bostonsinga@gmail.com>
2025-03-17 22:00:32 +0700

    Insert namespace 'mt_alg' to 'sorters' calls

--- test/number-test.cpp
+++ test/number-test.cpp
@@ -23,37 +23,37 @@ int main() {
   mt_uti::Timer timer;
 
   /** Bubble Sort */
-  mt_sor::Bubble<int, int>::solve(numbers, numbers_copy);
+  mt_alg::sorters::Bubble<int, int>::solve(numbers, numbers_copy);
   mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[1]);
   timer.print(true, "Bubble Sort:");
   std::cout << std::endl;
 
   /** Comb Sort */
-  mt_sor::Comb<int, int>::solve(numbers, numbers_copy);
+  mt_alg::sorters::Comb<int, int>::solve(numbers, numbers_copy);
   mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[2]);
   timer.print(true, "Comb Sort:");
   std::cout << std::endl;
 
   /** Insertion Sort */
-  mt_sor::Insertion<int, int>::solve(numbers, numbers_copy);
+  mt_alg::sorters::Insertion<int, int>::solve(numbers, numbers_copy);
   mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[3]);
   timer.print(true, "Insertion Sort:");
   std::cout << std::endl;
 
   /** Merge Sort */
-  mt_sor::Merge<int, int>::solve(numbers, numbers_copy);
+  mt_alg::sorters::Merge<int, int>::solve(numbers, numbers_copy);
   mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[4]);
   timer.print(true, "Merge Sort:");
   std::cout << std::endl;
 
   /** Quick Sort */
-  mt_sor::Quick<int, int>::solve(numbers, numbers_copy);
+  mt_alg::sorters::Quick<int, int>::solve(numbers, numbers_copy);
   mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[5]);
   timer.print(true, "Quick Sort:");
   std::cout << std::endl;
 
   /** Selection Sort */
-  mt_sor::Selection<int, int>::solve(numbers, numbers_copy);
+  mt_alg::sorters::Selection<int, int>::solve(numbers, numbers_copy);
   mt_uti::Printer<int>::logf(numbers_copy, true, false, filenames[6]);
   timer.print(true, "Selection Sort:");
   std::cout << std::endl;
