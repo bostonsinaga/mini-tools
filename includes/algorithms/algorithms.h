8a059f43f93be9424e0f1300c842c5807cf42399
bostonsinaga <bostonsinga@gmail.com>
2025-03-17 20:27:00 +0700

    Restored

---
+++ includes/algorithms/algorithms.h
@@ -0,0 +1,10 @@
+#ifndef __MINI_TOOLS__ALGORITHMS_H__
+#define __MINI_TOOLS__ALGORITHMS_H__
+
+#include "calculators.h"
+#include "equal-distribution.h"
+#include "sorters.h"
+
+namespace mt_sor = mini_tools::algorithms;
+
+#endif // __MINI_TOOLS__ALGORITHMS_H__
