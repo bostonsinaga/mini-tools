42b0a049e13534c56e81ffebdd35fc042739b221
bostonsinaga <bostonsinga@gmail.com>
2025-03-17 19:50:08 +0700

    Remove include to 'size-tools.h'

--- includes/data-structures/data-structures.h
+++ includes/data-structures/data-structures.h
@@ -1,7 +1,6 @@
 #ifndef __MINI_TOOLS__DATA_STRUCTURES_H__
 #define __MINI_TOOLS__DATA_STRUCTURES_H__
 
-#include "size-tools.h"
 #include "str-tools.h"
 #include "vec-tools.h"
 #include "linked-list.h"
