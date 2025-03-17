12e2d4fd6bf788a9438ac685cf971e0ccc9d842c
bostonsinaga <bostonsinga@gmail.com>
2025-03-17 19:40:51 +0700

    Moved all declarations to 'vec-tools.h'

--- includes/data-structures/size-tools.h
+++ includes/data-structures/size-tools.h
@@ -6,20 +6,10 @@ namespace data_structures {
 
   template <class T>
   class SizeTools {
-  private:
-    static size_t getMax(VEC_SZ &sizes);
-    static VEC_SZ getDifferences(VEC_SZ &sizes);
-    static VEC_LLI getDifferences(VEC_SZ &sizes, CR_LLI targetSize);
-    static void balance(CR_VEC_SZ differences, VEC2<T> &vecs, CR<T> coveringValue);
-    static void balance(CR_VEC_LLI differences, VEC2<T> &vecs, CR<T> coveringValue);
+  
 
   public:
-    static VEC_SZ generateSizes(CR_VEC2<T> vecs);
-    static size_t getMax(CR_VEC2<T> vecs);
-    static VEC_SZ getDifferences(CR_VEC2<T> vecs);
-    static VEC_LLI getDifferences(CR_VEC2<T> vecs, CR_LLI targetSize);
-    static void balance(VEC2<T> &vecs, CR<T> coveringValue);
-    static void balance(VEC2<T> &vecs, CR<T> coveringValue, LLI targetSize);
+    
   };
 }}
 
