4d57b271f244dce09b3618f580f91c980fb1c8f6
bostonsinaga <bostonsinga@gmail.com>
2025-03-18 11:48:08 +0700

    Add 'write' method. File writer
    Change methods parameters order

--- includes/utils/printer.h
+++ includes/utils/printer.h
@@ -8,21 +8,21 @@ namespace utils {
   class Printer {
   private:
     static std::string initText(
-      CR_INT step,
-      CR_STR title
+      CR_STR title,
+      CR_INT step
     );
 
     static std::string combine(
       CR_VEC<T> vec,
-      CR_INT step,
-      CR_STR title
+      CR_STR title,
+      CR_INT step
     );
 
     static std::string stringify(
       CR_VEC<T> vec,
       CR_BOL asBar,
-      CR_INT step,
-      CR_STR title
+      CR_STR title,
+      CR_INT step
     );
 
   public:
@@ -33,14 +33,29 @@ namespace utils {
       CR_INT step = -1
     );
 
+    /**
+     * Set the 'extended' parameter to determine
+     * whether to overwrite or continue the file.
+     */
+
     static void logf(
       CR_VEC<T> vec,
-      CR_BOL asBar,
-      CR_BOL extended, // 'false' will overwrite
       CR_STR filename,
+      CR_BOL asBar,
+      CR_BOL extended,
       CR_STR title = "Step",
       CR_INT step = -1
     );
+
+    static void write(
+      CR_VEC<T> vec,
+      CR_STR filename,
+      CR_BOL extended
+    ) {
+      Printer<T>::logf(
+        vec, filename, false, extended, ""
+      );
+    }
   };
 }}
 
