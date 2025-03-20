a95b6f139f2b99dfbe9939a34294014796151228
bostonsinaga <bostonsinga@gmail.com>
2025-03-20 10:45:54 +0700

    Change include 'equal-distribution.h' to 'round-robin.h'

--- includes/algorithms/algorithms.h
+++ includes/algorithms/algorithms.h
@@ -1,8 +1,8 @@
 #ifndef __MINI_TOOLS__ALGORITHMS_H__
 #define __MINI_TOOLS__ALGORITHMS_H__
 
-#include "calculators.h"
-#include "equal-distribution.h"
+#include "number-sequence.h"
+#include "round-robin.h"
 #include "sorters/sorters.h"
 
 namespace mt_alg = mini_tools::algorithms;
