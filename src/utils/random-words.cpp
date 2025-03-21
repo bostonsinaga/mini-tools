312e83e7d5146a4993d551cb79546529ebc2a0bf
bostonsinaga <bostonsinga@gmail.com>
2025-03-21 13:51:57 +0700

    Separate integrals and floating-point numbers generation

--- src/utils/random-words.cpp
+++ src/utils/random-words.cpp
@@ -1,77 +1,76 @@
-#include <iostream>
-#include <fstream>
-#include <cmath>
-#include <ctime>
+#include "random-words.h"
 
-void printSucceed() {
-  std::cout << "\nSeeds generated..\n";
-}
+namespace mini-tools {
+namespace utils {
 
-void printError() {
-  std::cerr
-    << "\nExpected inputs:\n"
-    << "   type<string>[number, text]\n"
-    << "   count<int>\n"
-    << "   maxNumber<int>\n"
-    << "   maxLetterCount<int>\n"
-    << "   outputFilename<string\n>";
-}
-
-void generateNumbers(std::ofstream &writer, int count, int maxNumber) {
-  for (int i = 0; i < count; i++) {
-    writer << std::rand() % maxNumber + 1 << ' ';
-  }
-  printSucceed();
-}
-
-void generateStrings(std::ofstream &writer, int count, int maxLetterCount) {
-
-  for (int i = 0; i < count; i++) {
-    for (int j = 0; j < std::rand() % maxLetterCount + 1; j++) {
-      bool isCapital = std::rand() % 2;
-      if (isCapital) writer << char(std::rand() % 26 + 65); // big
-      else writer << char(std::rand() % 26 + 97); // small
-    }
-    writer << ' ';
-  }
-
-  printSucceed();
-}
-
-// filename example: "resources/sample.txt"
-int main(int argc, char* argv[]) {
-
-  if (argc == 6) {
-    std::string type, outputFilename;
-    int count, maxNumber, maxLetterCount;
-
-    try {
-      type = argv[1];
-      count = std::stoi(argv[2]);
-      maxNumber = std::stoi(argv[3]);
-      maxLetterCount = std::stoi(argv[4]);
-      outputFilename = argv[5];
-    }
-    catch(...) {
-      printError();
-      return 0;
-    }
-
-    std::ofstream writer(outputFilename);
+  template <INTEGRALS_T T>
+  void RandomWords::generateIntegrals(
+    VEC<T> &integrals,
+    CR_SZ count,
+    T maxNumber,
+    CR_BOL needReset,
+    CR<T> additional
+  ) {
     std::srand(std::time(0));
+    if (needReset) integrals = {};
 
-    if (type == "number") {
-      generateNumbers(writer, count, maxNumber);
+    for (int i = 0; i < count; i++) {
+      integrals.push(
+        std::rand() % maxNumber + additional
+      );
     }
-    else if (type == "text") {
-      generateStrings(writer, count, maxLetterCount);
-    }
-    else printError();
-
-    writer.close();
   }
-  else printError();
 
-  return 0;
-}
+  template <FLOATING_POINT_T T>
+  void RandomWords::generateFloatingPoints(
+    VEC<T> floatingPoint,
+    CR_SZ count,
+    T maxNumber,
+    CR_BOL needReset,
+    CR<T> additional
+  ) {
+    std::srand(std::time(0));
+    if (needReset) floatingPoint = {};
+
+    for (int i = 0; i < count; i++) {
+      floatingPoint.push(std::fmod(
+        static_cast<double>(std::rand()) / RAND_MAX,
+        maxNumber) + additional
+      );
+    }
+  }
+
+  void RandomWords::generateStrings(
+    VEC<T> &strings,
+    CR_SZ count,
+    CR_SZ maxLettersCount,
+    CR_BOL needReset,
+    CR<T> additional
+  ) {
+    std::srand(std::time(0));
+    if (needReset) floatingPoint = {};
+
+    for (int i = 0; i < count; i++) {
+      for (int j = 0; j < std::rand() % maxLettersCount + 1; j++) {
+
+        bool isCapital = std::rand() % 2;
+
+        if (isCapital) { // big
+          strings.push(char(std::rand() % 26 + 65));
+        }
+        else { // small
+          strings.push(char(std::rand() % 26 + 97));
+        }
+      }
+
+      strings ' ';
+    }
+  }
+
+  void RandomWords::generateMixture(
+    
+  ) {
+
+  }
+}}
 
