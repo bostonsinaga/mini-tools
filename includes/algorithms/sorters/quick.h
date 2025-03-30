eade0460ca6910ec410fb91ac861cbfeac70d583
bostonsinaga <bostonsinga@gmail.com>
2025-03-30 14:46:24 +0700

    Set 'scheme' parameter as template parameter 'QUICK_SCHEME_ENUM'
    Change 'order' parameter type to 'CR_ORDER_ENUM'
    
    Unite 'messy' and 'attached' vector parameters as 'VEC_PAIR2' with 'messy' as the name

--- includes/algorithms/sorters/quick.h
+++ includes/algorithms/sorters/quick.h
@@ -5,61 +5,45 @@ namespace mini_tools {
 namespace algorithms {
 namespace sorters {
 
-  template <NUMBER T, class U = std::nullptr_t>
-  class Quick {
-  public:
-    enum SCHEME {HOARE, LOMUTO};
-    typedef const SCHEME& CR_SCHEME;
+  enum QUICK_SCHEME_ENUM {QUICK_HOARE, QUICK_LOMUTO};
 
+  template <inspector::NUMBER T, class U, QUICK_SCHEME_ENUM SCHEME = QUICK_HOARE>
+  class Quick {
   private:
     /** Slower */
     static int lomutoPartition(
-      VEC<T> &messy,
-      VEC<U> *attached,
+      VEC_PAIR2<T, U> &messy,
       CR_INT left,
       CR_INT right,
-      bool &ascending
+      CR_ORDER_ENUM order
     );
 
     /** Faster */
     static int hoarePartition(
-      VEC<T> &messy,
-      VEC<U> *attached,
+      VEC_PAIR2<T, U> &messy,
       CR_INT left,
       CR_INT right,
-      bool &ascending
+      CR_ORDER_ENUM order
     );
 
     static int randomPartition(
-      VEC<T> &messy,
-      VEC<U> *attached,
+      VEC_PAIR2<T, U> &messy,
       CR_INT left,
       CR_INT right,
-      bool &ascending,
-      CR_SCHEME scheme
+      CR_ORDER_ENUM order
     );
 
     static void recursion(
-      VEC<T> &messy,
-      VEC<U> *attached,
+      VEC_PAIR2<T, U> &messy,
       CR_INT left,
       CR_INT right,
-      bool &ascending,
-      CR_SCHEME scheme
+      CR_ORDER_ENUM order
     );
 
   public:
     static void solve(
-      VEC<T> &messy,
-      VEC<U> &attached,
-      bool ascending = true,
-      CR_SCHEME scheme = HOARE
-    );
-
-    static void solve(
-      VEC<T> &messy,
-      bool ascending = true,
-      CR_SCHEME scheme = HOARE
+      VEC_PAIR2<T, U> &messy,
+      CR_ORDER_ENUM order = ORDER_ASCENDING
     );
   };
 }}}
