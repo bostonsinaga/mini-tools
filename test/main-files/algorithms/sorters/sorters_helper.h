3674f0034d776413f2cf559ba3ffbbb850fd2832
bostonsinaga <bostonsinga@gmail.com>
2025-04-07 21:09:56 +0700

    Initial commit. Has reusable functions for this directory tests

---
+++ test/main-files/algorithms/sorters/sorters_helper.h
@@ -0,0 +1,27 @@
+#include "helper.h"
+
+namespace sorters_helper {
+
+  void usePair(mt::VEC_PAIR2<int, std::string> &merged);
+
+  helper::TUP4PTR use4Vectors(
+    mt::VEC_INT &integers,
+    mt::VEC_DBL &reals,
+    mt::VEC_STR &letters,
+    mt::VEC_STR &words
+  ) {
+    mt::VEC_PAIR2<int, std::string> merged
+      = mt_uti::PairTools<int, std::string>::mergeVectors(integers, letters);
+
+    usePair(merged);
+
+    mt_uti::PairTools<int, std::string>::unmergeVectors(
+      merged, integers, letters, true
+    );
+
+    return std::make_tuple(
+      &integers, nullptr, &letters, nullptr
+    );
+  }
+}
+
