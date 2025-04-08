cc4f67f575fc8e7a1441e1e2865647acc73201a5
bostonsinaga <bostonsinga@gmail.com>
2025-04-08 11:36:27 +0700

    Run executables from '../build' directory and print message

---
+++ build-scripts/run_tests/run_algorithms.cmd
@@ -0,0 +1,11 @@
+@echo off
+"../build/test/main-files/algorithms/sorters/algorithms_numbersequence.exe"
+"../build/test/main-files/algorithms/sorters/algorithms_roundrobin.exe"
+"../build/test/main-files/algorithms/sorters/algorithms_sorters_bubble.exe"
+"../build/test/main-files/algorithms/sorters/algorithms_sorters_comb.exe"
+"../build/test/main-files/algorithms/sorters/algorithms_sorters_insertion.exe"
+"../build/test/main-files/algorithms/sorters/algorithms_sorters_merge.exe"
+"../build/test/main-files/algorithms/sorters/algorithms_sorters_quick.exe"
+"../build/test/main-files/algorithms/sorters/algorithms_sorters_selection.exe"
+echo.
+powershell -command "& {Write-Host 'ALGORITHMS TESTS COMPLETED' -ForegroundColor Green}"
