9938c046ee488e7b6a727b365fafac1cab1fc67d
bostonsinaga <bostonsinga@gmail.com>
2025-04-08 15:35:21 +0700

    Initial commit. As a replacement to 'std::srand' and 'std::rand'

---
+++ includes/utils/random-words.h
@@ -0,0 +1,69 @@
+#ifndef __MINI_TOOLS__UTILS__RANDOM_WORDS_H__
+#define __MINI_TOOLS__UTILS__RANDOM_WORDS_H__
+
+namespace mini_tools {
+namespace utils {
+
+  class RandomWords final {
+  private:
+    static PAIR<LD> rouletteInterval;
+    static std::random_device randomDevice;
+    static std::mt19937 mersenneTwister;
+    static std::uniform_int_distribution<LLI> distributeInteger;
+    static std::uniform_real_distribution<LD> distributeReal;
+
+  public:
+    RandomWords() = delete;
+
+    static void setRouletteInterval(
+      CR_LD begin, CR_LD end
+    );
+
+    static void updateDistributeIntegerInterval();
+    static void updateDistributeRealInterval();
+
+    static void setDistributeIntegerInterval(
+      CR_LLI begin, CR_LLI end
+    );
+
+    static void setDistributeRealInterval(
+      CR_LD begin, CR_LD end
+    );
+
+    static VEC_STR generateStrings(
+      CR_SZ count,
+      PAIR<size_t> lengthInterval,
+      CR_BOL lettersOnly,
+      CR_BOL alwaysStartWithLetter
+    );
+
+    /** Call these to get a random number */
+
+    static LLI rouletteInteger() {
+      return distributeInteger(mersenneTwister);
+    }
+
+    static LD rouletteReal() {
+      return distributeReal(mersenneTwister);
+    }
+
+    /** The Random Words */
+
+    template <std::integral T>
+    static VEC<T> generateIntegrals(
+      CR_SZ count,
+      T maxNumber,
+      CR<T> additional
+    );
+
+    template <std::floating_point T>
+    static VEC<T> generateFloatingPoints(
+      CR_SZ count,
+      T maxNumber,
+      CR<T> additional
+    );
+  };
+}}
+
+#include "utils/random-words.tpp"
+#endif // __MINI_TOOLS__UTILS__RANDOM_WORDS_H__
