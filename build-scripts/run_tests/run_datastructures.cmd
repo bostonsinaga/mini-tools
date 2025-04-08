ddb7e89f61bd727efaae713e85fb1f9e40b6c934
bostonsinaga <bostonsinga@gmail.com>
2025-04-08 12:13:27 +0700

    Fix path to 'build' directory at root

--- build-scripts/run_tests/run_datastructures.cmd
+++ build-scripts/run_tests/run_datastructures.cmd
@@ -1,10 +1,10 @@
 @echo off
-"../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_circular.exe"
-"../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_circulardoubly.exe"
-"../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_doubly.exe"
-"../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_singly.exe"
-"../build/test/main-files/data-structures/trees/datastructures_trees_binarytree.exe"
-"../build/test/main-files/data-structures/trees/datastructures_trees_binary.exe"
-"../build/test/main-files/data-structures/trees/datastructures_trees_general.exe"
+"../../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_circular.exe"
+"../../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_circulardoubly.exe"
+"../../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_doubly.exe"
+"../../build/test/main-files/data-structures/linked-lists/datastructures_linkedlists_singly.exe"
+"../../build/test/main-files/data-structures/trees/datastructures_trees_binarytree.exe"
+"../../build/test/main-files/data-structures/trees/datastructures_trees_binary.exe"
+"../../build/test/main-files/data-structures/trees/datastructures_trees_general.exe"
 echo.
 powershell -command "& {Write-Host 'DATA_STRUCTURES TESTS COMPLETED' -ForegroundColor Green}"
--- build-scripts/run_tests/run_utils.cmd
+++ build-scripts/run_tests/run_utils.cmd
@@ -1,9 +1,9 @@
 @echo off
-"../build/test/main-files/utils/utils_printer.exe"
-"../build/test/main-files/utils/utils_randomwords.exe"
-"../build/test/main-files/utils/utils_scanner.exe"
-"../build/test/main-files/utils/utils_strtools.exe"
-"../build/test/main-files/utils/utils_timedate.exe"
-"../build/test/main-files/utils/utils_vectools.exe"
+"../../build/test/main-files/utils/utils_printer.exe"
+"../../build/test/main-files/utils/utils_randomwords.exe"
+"../../build/test/main-files/utils/utils_scanner.exe"
+"../../build/test/main-files/utils/utils_strtools.exe"
+"../../build/test/main-files/utils/utils_timedate.exe"
+"../../build/test/main-files/utils/utils_vectools.exe"
 echo.
 powershell -command "& {Write-Host 'UTILS TESTS COMPLETED' -ForegroundColor Green}"
