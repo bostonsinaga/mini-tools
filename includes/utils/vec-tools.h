5e009521eceb7c1be8fc39dd5143250dc32eef9c
bostonsinaga <bostonsinga@gmail.com>
2025-04-11 11:55:57 +0700

    Change 'cut..' methods to 'erase' and 'extract..' methods with option to keep the order (stable) but slower and disrupt the order (unstable) with faster
    Turn members duplication cleaners to a nested class with 'STABLE' and 'UNSTABLE' options to using 'extract..' methods
    
    Change 'int' variables type to 'size_t' and 'long long int'

--- includes/utils/vec-tools.h
+++ includes/utils/vec-tools.h
@@ -5,44 +5,25 @@ namespace mini_tools {
 namespace utils {
 
   template <class T>
-  class VecTools {
+  class VecTools final {
   public:
     typedef std::function<bool(CR<T>,CR<T>)> EQUAL_RULE;
 
   private:
     VecTools() = delete;
 
-    // invoked in the loop of 'clean..' method
-    static bool fillWastedDuplicateInside (
-      VEC<T> &vec, EQUAL_RULE &equalRule,
-      PAIR<VEC<T>> &wastedPair,
-      CR<T> &a, CR<T> &b, CR_UI cutIdx
-    );
-
-    /**
-     * The 'EQUAL_RULE' can be a lambda but it is better
-     * to be a separate method to avoid errors.
-     */
-    static bool fillWastedDuplicateToMember(
-      VEC<T> &vec, PAIR<VEC<T>> &wastedPair,
-      EQUAL_RULE &equalRule, bool &firstIgnored,
-      CR<T> &a, CR<T> &b, CR_UI cutIdx
-    );
-
-    //____________|
-    // SIZE TOOLS |
-    //____________|
+    /** SIZE TOOLS */
 
     static size_t getMin(VEC_SZ &sizes);
     static size_t getMax(VEC_SZ &sizes);
     static VEC_SZ getDifferences(VEC_SZ &sizes);
-    static VEC_LLI getDifferences(VEC_SZ &sizes, CR_LLI targetSize);
+    static VEC_LLI getDifferences(VEC_LLI &sizes, CR_LLI targetSize);
     static void balance(CR_VEC_SZ differences, VEC2<T> &vecs, CR<T> coveringValue);
     static void balance(CR_VEC_LLI differences, VEC2<T> &vecs, CR<T> coveringValue);
 
   public:
-    static bool hasIndex(VEC<T> &vec, CR_INT idx);
-    static bool hasIndexes(VEC<T> &vec, CR_VEC_INT idxs);
+    static bool hasIndex(VEC<T> &vec, CR_LLI idx);
+    static bool hasIndices(VEC<T> &vec, CR_VEC_LLI indices);
 
     // return -1 if not found
     static LLI getIndex(
@@ -54,116 +35,136 @@ namespace utils {
 
     static T getAt(
       VEC<T> &vec,
-      CR_INT idx,
+      CR_LLI idx,
       CR<T> defaultReturn
     );
 
-    static void concat(
+    /** VECTORS CONNECTORS */
+
+    /** Connect vector to vector */
+
+    static void concatCopy(
       VEC<T> &targetVec,
       CR_VEC<T> additionVec
     );
 
-    static VEC<T> join(
+    static void concatCut(
+      VEC<T> &targetVec,
+      VEC<T> &additionVec,
+      CR_BOL needShrink = true
+    );
+
+    /** Connect 2 vectors to a vector */
+
+    static VEC<T> joinCopy(
       CR_VEC<T> vecA,
       CR_VEC<T> vecB
     );
 
-    static void cutInterval(
-      VEC<T> &vec,
-      VEC<T> &wasted,
-      int begin, int end,
-      CR_BOL validatingIndex = false,
-      CR_BOL onlyWasted = false
+    static VEC<T> joinCut(
+      VEC<T> &vecA,
+      VEC<T> &vecB,
+      CR_BOL needShrink = true
     );
 
-    static VEC<T> cutInterval(
-      VEC<T> &vec,
-      CR_INT begin, CR_INT end,
-      CR_BOL validatingIndex = false,
-      CR_BOL onlyWasted = false
-    );
-
-    static void cutSingle(
-      VEC<T> &vec,
-      T &wasted,
-      int idx,
-      CR_BOL validatingIndex = false,
-      CR_BOL onlyWasted = false
-    );
-
-    static T cutSingle(
-      VEC<T> &vec,
-      CR_INT idx,
-      CR_BOL validatingIndex = false,
-      CR_BOL onlyWasted = false
-    );
+    // 'T' can also be a vector for multidimensional
+    static VEC<T> collapseCopy(CR_VEC2<T> vec);
+    static VEC<T> collapseCut(VEC2<T> &vec, CR_BOL needShrink = true);
 
     /**
-     * Set the 'lockedIndex' to true will maintain
-     * truncation targeting the initial member
-     * (the target index will not point to another member
-     * when one of the smaller indexes has finished truncating)
+     * NOTE:
+     * Make sure to use 'fixIndexInterval' or 'isIndexIntervalValid' methods
+     * before calling 'erase..' or 'extract..' to prevent undefined behavior
+     * when these methods accessing the 'vec' parameter.
      */
-    static void cutIndexes(
+
+    /** STABLE ERASERS (SLOWER) */
+
+    static void eraseSingleStable(
       VEC<T> &vec,
-      VEC<T> &wasted,
-      VEC_INT idxs,
-      CR_BOL lockedIndex = true,
-      CR_BOL validatingIndex = false,
-      CR_BOL onlyWasted = false
+      CR_SZ index,
+      T extracted
     );
 
-    static VEC<T> cutIndexes(
+    static void eraseIntervalStable(
       VEC<T> &vec,
-      CR_VEC_INT idxs,
-      CR_BOL lockedIndex = true,
-      CR_BOL validatingIndex = false,
-      CR_BOL onlyWasted = false
+      CR_PAIR<size_t> interval,
+      VEC<T> &extracted
     );
 
-    /**
-     * Compare each member internally.
-     * 
-     * Returns 2 different values.
-     * One based on 'pointer' equality
-     * and the other based on value equality.
-     */
-    static PAIR<VEC<T>> cleanDuplicateInside(
+    static void eraseIndicesStable(
       VEC<T> &vec,
-      CR_BOL originalAscending = true,
-      // repeated and compared 'T' parameters
-      EQUAL_RULE equalRule = [](CR<T> rep, CR<T> com)->bool { return false; }
+      CR_VEC<size_t> indices,
+      VEC<T> &extracted
     );
 
-    /**
-     * Compare all members with an external value
-     * or with one of its own members.
-     * 
-     * Returns the same as 'cleanDuplicateInside'.
-     */
-    static PAIR<VEC<T>> cleanDuplicateToMember(
-      VEC<T> &vec, CR<T> mem,
-      CR_BOL originalAscending = true,
-      // repeated and compared 'T' parameters
-      EQUAL_RULE equalRule = [](CR<T> rep, CR<T> com)->bool { return false; }
+    /** UNSTABLE ERASERS (FASTER) */
+
+    static void eraseSingleUnstable(
+      VEC<T> &vec,
+      CR_SZ index,
+      T &extracted
     );
 
-    static void fixIndexes(
+    static void eraseIntervalUnstable(
       VEC<T> &vec,
-      VEC_INT &idxs,
-      CR_BOL removed = true
+      CR_PAIR<size_t> interval,
+      VEC<T> &extracted
+    );
+
+    static void eraseIndicesUnstable(
+      VEC<T> &vec,
+      CR_VEC<size_t> indices,
+      VEC<T> &extracted
+    );
+
+    /** STABLE EXTRACTORS */
+
+    static T extractSingleStable(
+      VEC<T> &vec,
+      CR_SZ index
+    );
+
+    static VEC<T> extractIntervalStable(
+      VEC<T> &vec,
+      CR_PAIR<size_t> interval
+    );
+
+    static VEC<T> extractIndicesStable(
+      VEC<T> &vec,
+      CR_VEC<size_t> indices
+    );
+
+    /** UNSTABLE EXTRACTORS */
+
+    static T extractSingleUnstable(
+      VEC<T> &vec,
+      CR_SZ index
+    );
+
+    static VEC<T> extractIntervalUnstable(
+      VEC<T> &vec,
+      CR_PAIR<size_t> interval
+    );
+
+    static VEC<T> extractIndicesUnstable(
+      VEC<T> &vec,
+      CR_VEC<size_t> indices
+    );
+
+    /** INDEX LIMITERS */
+
+    static void fixIndices(
+      VEC<T> &vec,
+      VEC_LLI &indices,
+      CR_BOL needRemove = true
     );
 
     // range of values ​​from 0 to size - 1
-    static void fixIndexInterval(VEC<T> &vec, int &begin, int &end);
-    static bool isIndexIntervalValid(VEC<T> &vec, CR_INT begin, CR_INT end);
+    static void fixIndexInterval(VEC<T> &vec, LLI &begin, LLI &end);
+    static bool isIndexIntervalValid(VEC<T> &vec, CR_LLI begin, CR_LLI end);
 
-    // 'T' can also be a vector for multidimensional
-    static VEC<T> collapse(CR_VEC2<T> vec);
-
-    //____________|
-    // SIZE TOOLS |
-    //____________|
+    /** SIZE TOOLS */
 
     static VEC_SZ generateSizes(CR_VEC2<T> vecs);
     static size_t getMin(CR_VEC2<T> vecs);
@@ -172,6 +173,43 @@ namespace utils {
     static VEC_LLI getDifferences(CR_VEC2<T> vecs, CR_LLI targetSize);
     static void balance(VEC2<T> &vecs, CR<T> coveringValue);
     static void balance(VEC2<T> &vecs, CR<T> coveringValue, LLI targetSize);
+
+    /** MEMBER DUPLICATION CLEANERS */
+
+    enum DUPLICATION_ENUM {
+      DUPLICATION_STABLE, DUPLICATION_UNSTABLE
+    };
+
+    template<DUPLICATION_ENUM U>
+    class Duplication final {
+    private:
+      static bool fillWasted(
+        VEC<T> &vec,
+        VEC<T> &wasted,
+        EQUAL_RULE &equalRule,
+        CR<T> &a,
+        CR<T> &b,
+        CR_SZ cutIdx
+      );
+
+    public:
+      /** Keep only one member of the same */
+      static VEC<T> eliminate(
+        VEC<T> &vec,
+        CR_BOL originalAscending = true,
+        // repeated and compared 'T' parameters
+        EQUAL_RULE equalRule = [](CR<T> rep, CR<T> com)->bool { return false; }
+      );
+
+      /** Remove all members that are equal to an external value */
+      static VEC<T> clean(
+        VEC<T> &vec,
+        CR<T> val,
+        CR_BOL originalAscending = true,
+        // repeated and compared 'T' parameters
+        EQUAL_RULE equalRule = [](CR<T> rep, CR<T> com)->bool { return false; }
+      );
+    };
   };
 }}
 
