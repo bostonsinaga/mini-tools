4b4460f490b442d9f9542087aa6dc0de279ec55f
bostonsinaga <bostonsinga@gmail.com>
2025-04-15 13:13:29 +0700

    Fix path to executable and add more arguments to it

--- build-scripts/run_tests/init_inputs.cmd
+++ build-scripts/run_tests/init_inputs.cmd
@@ -1,3 +1,4 @@
 @echo off
-"../build/test/main-files/utils/utils_randomwords.exe --init-inputs"
+"../../build/test/main-files/utils/utils_randomwords.exe" --init-inputs -amount 1000 -max-number 100 -word-length-start 5 -word-length-end 11
+echo.
 powershell -command "& {Write-Host 'INPUTS INITIALIZED' -ForegroundColor Green}"
