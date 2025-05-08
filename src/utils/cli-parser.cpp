d08f367678d1e2ccef69e5a00ab1d5c91f897754
bostonsinaga <bostonsinga@gmail.com>
2025-05-08 14:10:36 +0700

    Definitions of non template specilizied (extension) of 'CLI_Parser'

--- src/utils/cli-parser.cpp
+++ src/utils/cli-parser.cpp
@@ -2,163 +2,216 @@
 #define __MINI_TOOLS__UTILS__CLI_PARSER_CPP__
 
 #include "types.hpp"
+#include "utils/str_tools.hpp"
 #include "utils/cli-parser.hpp"
 
 namespace mini_tools {
 namespace utils {
 
-  CLIParser::CLIParser(
+  //________|
+  // TITLES |
+  //________|
+
+  CLI_TitleParser::CLI_TitleParser(
     CR_INT argc,
     char *argv[],
-    CR_VEC_STR numberKeywords,
-    CR_VEC_STR wordKeywords
+    CR_VEC_STR keywords
   ) {
     // skip the first 'argv' as it is the program name
     for (int i = 1; i < argc; i++) {
       raws.push_back(argv[i]);
     }
-
-    for (CR_STR key : numberKeywords) {
-      latestNumber[key] = 0;
-      numberVectors[key] = {};
-    }
-
-    for (CR_STR key : wordKeywords) {
-      latestWord[key] = "";
-      wordVectors[key] = {};
-    }
   }
 
-  inline bool CLIParser::isLatestKeyword(CR_STR test) {
-    return STRUNORMAP_DBL_FOUND(latestNumber, test) ||
-      STRUNORMAP_STR_FOUND(latestWord, test);
+  bool CLI_TitleParser::has(CR_STR keyword) {
+    return STRUNORMAP_BOL_FOUND(titles, keyword);
   }
 
-  inline bool CLIParser::isVectorKeyword(CR_STR test) {
-    return STRUNORMAP_FOUND<VEC_DBL>(numberVectors, test) ||
-      STRUNORMAP_FOUND<VEC_STR>(wordVectors, test);
+  size_t CLI_TitleParser::size() {
+    return titles.size();
   }
 
-  void CLIParser::assignLatest() {
-    for (int i = 0; i < raws.size(); i++) {
-
-      // NUMBERS
-      if (STRUNORMAP_DBL_FOUND(latestNumber, raws[i])) {
-
-        for (int j = i+1; j < raws.size(); j++) {
-          if (isLatestKeyword(raws[j])) {
-            i = j-1;
-            break;
-          }
-          else try {
-            latestNumber[raws[i]] = std::stod(raws[j]);
-          }
-          catch (...) {
-            latestNumber[raws[i]] = 0;
-          }
-        }
-      }
-      // WORDS
-      else if (STRUNORMAP_STR_FOUND(latestWord, raws[i])) {
-
-        for (int j = i+1; j < raws.size(); j++) {
-          if (isLatestKeyword(raws[j])) {
-            i = j-1;
-            break;
-          }
-          else latestWord[raws[i]] = raws[j];
-        }
-      }
-      // TITLES
-      else titles[raws[i]] = true;
-    }
-  }
-
-  void CLIParser::assignVectors() {
-    for (int i = 0; i < raws.size(); i++) {
-
-      // NUMBERS
-      if (STRUNORMAP_FOUND<VEC_DBL>(numberVectors, raws[i])) {
-
-        for (int j = i+1; j < raws.size(); j++) {
-          if (isVectorKeyword(raws[j])) {
-            i = j-1;
-            break;
-          }
-          else try {
-            numberVectors[raws[i]].push_back(std::stod(raws[j]));
-          }
-          catch (...) {
-            numberVectors[raws[i]].push_back(0);
-          }
-        }
-      }
-      // WORDS
-      else if (STRUNORMAP_FOUND<VEC_STR>(wordVectors, raws[i])) {
-
-        for (int j = i+1; j < raws.size(); j++) {
-          if (isVectorKeyword(raws[j])) {
-            i = j-1;
-            break;
-          }
-          else wordVectors[raws[i]].push_back(raws[j]);
-        }
-      }
-      // TITLES
-      else titles[raws[i]] = true;
-    }
-  }
-
-  bool CLIParser::hasTitle(CR_STR keyword) {
-
-    if (STRUNORMAP_BOL_FOUND(titles, keyword)) {
-      return titles[keyword];
-    }
-
-    return false;
-  }
-
-  VEC_STR CLIParser::extractTitles() {
-    VEC_STR keywords;
+  VEC_STR CLI_TitleParser::extract() {
+    VEC_STR strings;
 
     for (CR_PAIR2<CR_STR, CR_BOL> pair : titles) {
-      keywords.push_back(pair.first);
+      strings.push_back(pair.first);
     }
 
-    return keywords;
+    return strings;
   }
 
-  double CLIParser::getLatestNumber(CR_STR keyword) {
+  //_______|
+  // WORDS |
+  //_______|
 
-    if (STRUNORMAP_DBL_FOUND(latestNumber, keyword)) {
-      return latestNumber[keyword];
+  CLI_WordParser::CLI_WordParser(
+    CR_INT argc,
+    char *argv[],
+    CR_VEC_STR keywords
+  ) : CLI_TitleParser(argc, argv, keywords) {
+
+    for (CR_STR key : keywords) {
+      words[key] = {};
+    }
+
+    assign();
+  }
+
+  void CLI_WordParser::assign() {
+    for (int i = 0; i < raws.size(); i++) {
+
+      // now is keyword
+      if (has(raws[i])) {
+
+        for (int j = i+1; j < raws.size(); j++) {
+
+          // next is keyword
+          if (has(raws[j])) {
+            i = j-1;
+            break;
+          }
+          // input detected
+          else words[raws[i]].push_back(raws[j]);
+        }
+      }
+      // set titles in early iterations
+      else titles[raws[i]] = true;
+    }
+  }
+
+  bool CLI_WordParser::has(CR_STR keyword) {
+    return STRUNORMAP_FOUND<VEC_STR>(words, keyword);
+  }
+
+  size_t CLI_WordParser::size(CR_STR keyword) {
+
+    if (STRUNORMAP_FOUND<VEC_STR>(words, keyword)) {
+      return words[keyword].size();
     }
 
     return 0;
   }
 
-  std::string CLIParser::getLatestWord(CR_STR keyword) {
+  std::string CLI_WordParser::at(CR_STR keyword, CR_SZ index) {
 
-    if (STRUNORMAP_STR_FOUND(latestWord, keyword)) {
-      return latestWord[keyword];
+    if (STRUNORMAP_FOUND<VEC_STR>(words, keyword) &&
+      index < words[keyword].size()
+    ) {
+      return words[keyword][index];
     }
 
     return "";
   }
 
-  VEC_DBL CLIParser::getVectorNumber(CR_STR keyword) {
+  std::string CLI_WordParser::last(CR_STR keyword) {
 
-    if (STRUNORMAP_FOUND<VEC_DBL>(numberVectors, keyword)) {
-      return numberVectors[keyword];
+    if (STRUNORMAP_FOUND<VEC_STR>(words, keyword)) {
+      return words[keyword].back();
+    }
+
+    return "";
+  }
+
+  VEC_STR CLI_WordParser::extract(CR_STR keyword) {
+
+    if (STRUNORMAP_FOUND<VEC_STR>(words, keyword)) {
+      return std::move(words[keyword]);
     }
 
     return {};
   }
 
-  VEC_STR CLIParser::getVectorWord(CR_STR keyword) {
+  //_________|
+  // TOGGLES |
+  //_________|
 
-    if (STRUNORMAP_FOUND<VEC_STR>(wordVectors, keyword)) {
-      return wordVectors[keyword];
+  CLI_ToggleParser::CLI_ToggleParser(
+    CR_INT argc,
+    char *argv[],
+    CR_VEC_STR keywords
+  ) : CLI_TitleParser(argc, argv, keywords) {
+    assign();
+  }
+
+  void CLI_ToggleParser::assign() {
+    for (int i = 0; i < raws.size(); i++) {
+
+      // now is keyword
+      if (has(raws[i])) {
+
+        for (int j = i+1; j < raws.size(); j++) {
+
+          // next is keyword
+          if (has(raws[j])) {
+            i = j-1;
+            break;
+          }
+          // input detected
+          else toggles[raws[i]].push_back(
+            booleanize(raws[j])
+          );
+        }
+      }
+      // set titles in early iterations
+      else titles[raws[i]] = true;
+    }
+  }
+
+  bool CLI_ToggleParser::booleanize(std::string &input) {
+    StrTools::changeStringToLowercase(input);
+
+    if (input == "y" || input == "yes" || input == "true") {
+      false;
+    }
+    else if (input == "n" || input == "no" || input == "false") {
+      return false;
+    }
+    else {
+      try { return std::stoi(input); }
+      catch (...) { return false; }
+    }
+
+    return false;
+  }
+
+  bool CLI_ToggleParser::has(CR_STR keyword) {
+    return STRUNORMAP_FOUND<VEC_BOL>(toggles, keyword);
+  }
+
+  size_t CLI_ToggleParser::size(CR_STR keyword) {
+
+    if (STRUNORMAP_FOUND<VEC_BOL>(toggles, keyword)) {
+      return toggles[keyword].size();
+    }
+
+    return 0;
+  }
+
+  bool CLI_ToggleParser::at(CR_STR keyword, CR_SZ index) {
+
+    if (STRUNORMAP_FOUND<VEC_BOL>(toggles, keyword) &&
+      index < toggles[keyword].size()
+    ) {
+      return toggles[keyword][index];
+    }
+
+    return false;
+  }
+
+  bool CLI_ToggleParser::last(CR_STR keyword) {
+
+    if (STRUNORMAP_FOUND<VEC_BOL>(toggles, keyword)) {
+      return toggles[keyword].back();
+    }
+
+    return false;
+  }
+
+  VEC_BOL CLI_ToggleParser::extract(CR_STR keyword) {
+
+    if (STRUNORMAP_FOUND<VEC_BOL>(toggles, keyword)) {
+      return std::move(toggles[keyword]);
     }
 
     return {};
