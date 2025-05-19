4ff049ba5b9c7478b4dd1286b3f0cef4c5537e27
bostonsinaga <bostonsinga@gmail.com>
2025-05-19 07:33:21 +0700

    Rename includes

--- includes/data_structures/data_structures.hpp
+++ includes/data_structures/data_structures.hpp
@@ -1,8 +1,8 @@
 #ifndef __MINI_TOOLS__DATA_STRUCTURES_HPP__
 #define __MINI_TOOLS__DATA_STRUCTURES_HPP__
 
-#include "linked_list.hpp"
-#include "tree.hpp"
+#include "linked-list.hpp"
+#include "general-tree.hpp"
 
 namespace mt_ds = mini_tools::data_structures;
 
