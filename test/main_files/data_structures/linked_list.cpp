cdb2e7cf6930b742a913c2805a307992bbc14c50
bostonsinaga <bostonsinga@gmail.com>
2025-05-19 07:31:44 +0700

    Add 'iterateNodes' function for reusable iteration
    Test 'slice' and 'merge' methods

--- test/main_files/data_structures/linked_list.cpp
+++ test/main_files/data_structures/linked_list.cpp
@@ -1,7 +1,9 @@
 #include "mini_tools.hpp"
 #include "helper.hpp"
 
-class Node : public mt_ds::LinkedList {
+typedef mt_ds::LinkedList LL;
+
+class Node : public LL {
 public:
   std::string name;
 
@@ -14,44 +16,80 @@ public:
   }
 };
 
+void iterateNodes(
+  const LL::DIRECTION &dir,
+  Node* &current,
+  mt::CR_STR title
+) {
+  std::cout << "\n" + title + ":\n";
+
+  current->head()->iterate(dir, [](LL *object)->bool {
+    static_cast<Node*>(object)->print();
+    return true;
+  });
+}
+
 int main(int argc, char *argv[]) {
 
   mt_uti::CLI_Parser<int> parser(
     argc, argv, {"-name"}, {"-amount"}
   );
 
+  // minimum value is 1
+  int count = parser.CLI_NumberParser::last("-amount");
+
+  // title as entry point
   if (parser.CLI_TitleParser::has("--test")) {
 
-    Node *current = new Node(
-      parser.CLI_WordParser::last("-name") + " 1"
-    );
+    if (!count) { // the amount is 0
+      std::cerr << "\nPlease specify the 'amount' parameter\n";
+      return 0;
+    }
 
-    for (int i = 1; i < parser.CLI_NumberParser::last("-amount"); i++) {
+    Node *middle = nullptr;
+    std::string name = parser.CLI_WordParser::last("-name");
+
+    if (!name.empty()) name += ' ';
+
+    // initialization
+    Node *current = new Node(name + "1");
+
+    // connecting
+    for (int i = 1; i < count; i++) {
       current->accept(new Node(
-        parser.CLI_WordParser::last("-name") + ' ' + std::to_string(i+1)
+        name + std::to_string(i+1)
       ));
 
+      if (i == count / 2) middle = current;
       current = static_cast<Node*>(current->next());
     }
 
-    if (current) {
-      std::cout << "\niterateRight:\n";
+    if (current && middle) {
 
-      current->head()->iterateRight([](mt_ds::LinkedList *object)->bool {
-        static_cast<Node*>(object)->print();
-        return true;
-      });
+      // initial iterations
+      iterateNodes(LL::RIGHT, current, "Initial[RIGHT]");
+      iterateNodes(LL::LEFT, current, "Initial[LEFT]");
 
-      std::cout << "\niterateLeft:\n";
+      /** Slice */
 
-      current->tail()->iterateLeft([](mt_ds::LinkedList *object)->bool {
-        static_cast<Node*>(object)->print();
-        return true;
-      });
+      Node *sliced = static_cast<Node*>(middle->slice());
 
-      current->annihilate();
+      if (sliced) {
+        iterateNodes(LL::RIGHT, sliced, "Sliced");
+        iterateNodes(LL::RIGHT, middle, "Middle");
+      }
+
+      /** Merge */
+
+      middle->merge(sliced);
+      iterateNodes(LL::RIGHT, middle, "Middle merged to Sliced");
     }
+    else std::cout << "\nJust a lonely " + name + "1\n";
+
+    // deallocation
+    current->annihilate();
   }
+  else std::cerr << "\nUnknown entry title\n";
 
   return 0;
 }
