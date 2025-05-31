10e6168f28e2b81f8acc85143af5c6ef0918746a
bostonsinaga <bostonsinga@gmail.com>
2025-05-31 19:50:38 +0700

    Add 'countDecimalPlaces' and 'countDigits' methods

--- includes/algorithms/number-sequence.hpp
+++ includes/algorithms/number-sequence.hpp
@@ -66,6 +66,42 @@ namespace algorithms {
       int partialTestCount = 0,
       int skipTestCount = 0
     );
+
+    /**
+     * You can sum this return with the return of 'countDecimalPlaces'
+     * to get the full number of digits and +1 for decimal point sign.
+     */
+    template <inspector::INTEGRAL T>
+    static int countDigits(CR<T> number) {
+      return (number == 0) ? 1 : std::log10(std::abs(number)) + 1;
+    }
+
+    /**
+     * Floating point precisions based on IEEE 754 standard:
+     * - Float = 6-7 digits.
+     * - Double = 15-17 digits.
+     * - Long Double = 19-21 digits.
+     * 
+     * You can specify the 'precision'
+     * with 'PRECISION_ENUM' from 'types.hpp'
+     * which stores the above standards.
+     */
+    template <inspector::FLOATING_POINT T>
+    static int countDecimalPlaces(
+      T number,
+      CR_USI precision = 6
+    ) {
+      int count = 0;
+
+      while (count < precision &&
+        number != static_cast<int>(number)
+      ) {
+        number *= 10;
+        count++;
+      }
+
+      return count;
+    }
   };
 }}
 
