1a062216f3afb1b647f56a0cc200882f70e548d3
bostonsinaga <bostonsinga@gmail.com>
2025-06-14 15:05:53 +0700

    Change use purpose to list product name, price, and edibility because PIN that started with '0' will ignored and will look like a bug, so its better to change it
    Clean unordered maps separately with print the size before cleaning

--- test/main-files/utils/cli-parser.cpp
+++ test/main-files/utils/cli-parser.cpp
@@ -5,61 +5,69 @@ int main(int argc, char *argv[]) {
   helper::CLIWrapper<int>(
     // help and error message
     helper::Helperr(
-      "Register username, pin, and premium status in sequence.",
-      "--test -username<WORD> -pin<NUMBER> -premium<TOGGLE>"
+      "Enter product name, price, and edibility in sequence.",
+      "--test -name<WORD> -price<NUMBER> -edible<TOGGLE>"
     ),
     // CLI registration
     mt_uti::CLIParser<int>(
       mt_uti::CLIParser<int>::argvToStringVector(argc, argv),
-      {std::make_pair("-username", "Noname")},
-      {std::make_pair("-pin", 0)},
-      {std::make_pair("--premium", false)}
+      {std::make_pair("-name", "Noname")},
+      {std::make_pair("-price", 0)},
+      {std::make_pair("--edible", true)}
     ),
-    // on 'cli.enter("--test")'
+    // on 'cli->enter("--test")'
     [](mt_uti::CLIParser<int> *cli)->bool {
       cli->balance(
-        {std::make_pair("-username", "")},
-        {std::make_pair("-pin", 0)},
-        {std::make_pair("--premium", false)}
+        {std::make_pair("-name", "Unknown")},
+        {std::make_pair("-price", 0)},
+        {std::make_pair("--edible", false)}
       );
 
       // word input
-      std::cout << "\nUsername:\n";
+      std::cout << "\nName:\n";
 
-      for (mt::CR_STR username : cli->getVectorAtWords("-username")) {
-        std::cout << "  " << username << std::endl;
+      for (mt::CR_STR name : cli->getVectorAtWords("-name")) {
+        std::cout << "  " << name << std::endl;
       }
 
       // number input
-      std::cout << "\nPIN:\n";
+      std::cout << "\nPrice:\n";
 
-      for (mt::CR<int> pin : cli->getVectorAtNumbers("-pin")) {
+      for (mt::CR<int> price : cli->getVectorAtNumbers("-price")) {
 
-        if (pin == 0) {
-          std::cout << "  " << "empty" << std::endl;
+        if (price <= 0) {
+          std::cout << "  " << "free" << std::endl;
         }
-        else if (mt_alg::NumberSequence::countDigits<int>(pin) != 6) {
-          std::cout << "  " << "invalid" << std::endl;
-        }
-        else std::cout << "  " << pin << std::endl;
+        else std::cout << "  " << price << std::endl;
       }
 
       // toggle input
-      std::cout << "\nPremium:\n";
+      std::cout << "\nEdibility:\n";
 
-      for (mt::CR_BOL premium : cli->getVectorAtToggles("-premium")) {
-        std::cout << "  " << premium << std::endl;
+      for (mt::CR_BOL edible : cli->getVectorAtToggles("--edible")) {
+        std::cout << "  " << (edible ? "Edible" : "Not Edible") << std::endl;
       }
 
       // clear and release all unordered maps
-      cli->cleanAll(true);
-      std::cout << "\n\033[3mAll unordered maps are cleared and released\033[0m\n";
+      std::cout << "\nUNORDERED MAP SIZES AND CLEANED:\n";
 
-      // show proof of cleaning
-      std::cout << " Entries Size = " << cli->getEntriesSize() << std::endl;
-      std::cout << " Words Size = " << cli->getWordsSize() << std::endl;
-      std::cout << " Numbers Size = " << cli->getNumbersSize() << std::endl;
-      std::cout << " Toggles Size = " << cli->getTogglesSize() << std::endl;
+      /** Show proofs of cleaning */
+
+      std::cout << " Entries from " << cli->getEntriesSize();
+      cli->cleanEntries(true);
+      std::cout << " to " << cli->getEntriesSize() << std::endl;
+
+      std::cout << " Words from " << cli->getSizeAtWords("-name");
+      cli->cleanWords(true);
+      std::cout << " to " << cli->getSizeAtWords("-name") << std::endl;
+
+      std::cout << " Numbers from " << cli->getSizeAtNumbers("-price");
+      cli->cleanNumbers(true);
+      std::cout << " to " << cli->getSizeAtNumbers("-price") << std::endl;
+
+      std::cout << " Toggles from " << cli->getSizeAtToggles("--edible");
+      cli->cleanToggles(true);
+      std::cout << " to " << cli->getSizeAtToggles("--edible") << std::endl;
 
       return true;
     }
