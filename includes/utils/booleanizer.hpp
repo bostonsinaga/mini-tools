492eb06de2f6034568993538703c71b28ef44ea2
bostonsinaga <bostonsinga@gmail.com>
2025-07-12 09:53:50 +0700

    Initial commit. Separation from 'cli-parser' file

---
+++ includes/utils/booleanizer.hpp
@@ -0,0 +1,68 @@
+#ifndef __MINI_TOOLS__UTILS__BOOLEANIZER_HPP__
+#define __MINI_TOOLS__UTILS__BOOLEANIZER_HPP__
+
+#include "types.hpp"
+#include "scanner.hpp"
+#include "str-tools.hpp"
+
+namespace mini_tools{
+namespace utils {
+
+  /**
+   * Interpret string as boolean.
+   */
+  class Booleanizer {
+  private:
+    /**
+     * UNORDERED MAPS
+     * Default is english.
+     * 
+     * All strings are expected to be interpreted
+     * as representing the boolean values.
+     * 
+     * The 'falseTerms' is actually used as example of false input in
+     * description of user class. The tester only evaluates 'trueTerms'.
+     */
+    STRUNORMAP<VEC_STR>
+      trueTerms = {{ "en", {"TRUE", "YES", "Y"} }},
+      falseTerms = {{ "en", {"FALSE", "NO", "N"} }};
+
+  public:
+    Booleanizer() {}
+    VEC_STR getTrueTerms(CR_STR existingISOCode);
+    VEC_STR getFalseTerms(CR_STR existingISOCode);
+
+    /**
+     * Compare each selected 'trueTerms' vector with 'input'.
+     * Return false if the input is interpreted as:
+     * - The string 'false'
+     * - The numeric value zero
+     * - A term in 'falseTerms'
+     * - Or a term not included in the allowed list
+     */
+    bool test(
+      CR_STR existingISOCode,
+      std::string input
+    );
+
+    /** Modify extension for other languages */
+
+    bool hasISOCode(CR_STR existingISOCode);
+
+    void addTerms(
+      CR_STR newISOCode,
+      CR_VEC_STR newTrueTerms,
+      CR_VEC_STR newFalseTerms
+    );
+
+    void changeTerms(
+      CR_STR existingISOCode,
+      CR_VEC_STR existingTrueTerms,
+      CR_VEC_STR existingFalseTerms
+    );
+
+    void removeTerms(CR_STR existingISOCode);
+  };
+}}
+
+#endif // __MINI_TOOLS__UTILS__CLI_PARSER_TPP__
---
+++ src/utils/booleanizer.cpp
@@ -0,0 +1,80 @@
+#ifndef __MINI_TOOLS__UTILS__BOOLEANIZER_CPP__
+#define __MINI_TOOLS__UTILS__BOOLEANIZER_CPP__
+
+#include "utils/booleanizer.hpp"
+
+namespace mini_tools{
+namespace utils {
+
+  VEC_STR Booleanizer::getTrueTerms(CR_STR existingISOCode) {
+    if (hasISOCode(existingISOCode)) {
+      return trueTerms[existingISOCode];
+    }
+    return {};
+  }
+
+  VEC_STR Booleanizer::getFalseTerms(CR_STR existingISOCode) {
+    if (hasISOCode(existingISOCode)) {
+      return falseTerms[existingISOCode];
+    }
+    return {};
+  }
+
+  // only evaluate the 'trueTerms' to return true
+  bool Booleanizer::test(
+    CR_STR existingISOCode,
+    std::string input
+  ) {
+    // 'input' is zero
+    if (!Scanner::stringToNumber<int>(input)) {
+
+      utils::StrTools::modifyStringToUppercase(input);
+
+      if (hasISOCode(existingISOCode)) {
+        for (CR_STR term : trueTerms[existingISOCode]) {
+          if (input == term) return true;
+        }
+      }
+
+      // 'input' does not match any 'trueTerms'
+      return false;
+    }
+
+    // 'input' is not zero
+    return true;
+  }
+
+  bool Booleanizer::hasISOCode(CR_STR existingISOCode) {
+    return STRUNORMAP_FOUND<VEC_STR>(trueTerms, existingISOCode);
+  }
+
+  void Booleanizer::addTerms(
+    CR_STR newISOCode,
+    CR_VEC_STR newTrueTerms,
+    CR_VEC_STR newFalseTerms
+  ) {
+    trueTerms[newISOCode] = newTrueTerms;
+    falseTerms[newISOCode] = newFalseTerms;
+  }
+
+  void Booleanizer::changeTerms(
+    CR_STR existingISOCode,
+    CR_VEC_STR existingTrueTerms,
+    CR_VEC_STR existingFalseTerms
+  ) {
+    if (hasISOCode(existingISOCode)) {
+      addTerms(
+        existingISOCode,
+        existingTrueTerms,
+        existingFalseTerms
+      );
+    }
+  }
+
+  void Booleanizer::removeTerms(CR_STR existingISOCode) {
+    trueTerms.erase(existingISOCode);
+    falseTerms.erase(existingISOCode);
+  }
+}}
+
+#endif // __MINI_TOOLS__UTILS__BOOLEANIZER_CPP__
