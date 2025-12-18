2685224e1425e56d46a6d5b8ba2c65b01ad3a1b7
bostonsinaga <bostonsinga@gmail.com>
2025-12-18 17:02:53 +0700

    Improvement for 'messageCallback' function. Detect the order of '-completed' and '-failed' keyword. Will only display the first detected one. If any of them does not exist the message will not be displayed
    Use single 'mt_uti::CLIParser<std::string>' for '-completed', '-failed', and new added '-border-style'
    
    Rename of 'StatusPrinter' properties

--- test/main-files/status-printer.cpp
+++ test/main-files/status-printer.cpp
@@ -6,60 +6,89 @@
 class StatusPrinter {
 private:
   inline static const std::string
-    defaultCompletedLabel = "PROGRAM COMPLETED",
-    defaultFailedLabel = "PROGRAM FAILED",
     coloringStrings[2] = {
       "\n\x1b[38;2;0;0;255m",  // blue
       "\n\x1b[38;2;255;127;0m" // orange
     };
 
 public:
-  enum LABEL_CODE {
-    LABEL_COMPLETED, LABEL_FAILED
+  enum STATUS_CODE {
+    STATUS_COMPLETED, STATUS_FAILED
   };
 
-  static void printStatusLabel(
-    mt_uti::CLIParser<std::string> &cli,
-    LABEL_CODE colorCode
+  static void displayStatusMessage(
+    mt::CR_VEC_STR words,
+    mt::CR_STR borderStyle,
+    STATUS_CODE colorCode
   ) {
     // clamping code value
-    if (colorCode < LABEL_CODE::LABEL_COMPLETED) {
-      colorCode = LABEL_CODE::LABEL_COMPLETED;
+    if (colorCode < STATUS_CODE::STATUS_COMPLETED) {
+      colorCode = STATUS_CODE::STATUS_COMPLETED;
     }
-    else if (colorCode > LABEL_CODE::LABEL_FAILED) {
-      colorCode = LABEL_CODE::LABEL_FAILED;
+    else if (colorCode > STATUS_CODE::STATUS_FAILED) {
+      colorCode = STATUS_CODE::STATUS_FAILED;
     }
 
     // opening color
-    std::cout << StatusPrinter::coloringStrings[colorCode];
+    std::cout << StatusPrinter::coloringStrings[colorCode] << borderStyle;
 
-    // get CLI input
-    mt::VEC_STR labels = cli.getVectorAt<std::string>("-message");
-
-    // print default labels
-    if (labels.empty()) {
-      if (colorCode == LABEL_CODE::LABEL_COMPLETED) {
-        std::cout << StatusPrinter::defaultCompletedLabel;
-      }
-      else std::cout << StatusPrinter::defaultFailedLabel;
-    }
-    // print labels from CLI input
-    else for (mt::CR_STR str : labels) {
-      std::cout << str << ' ';
+    // print words from CLI input
+    for (int i = 0; i < words.size(); i++) {
+      std::cout << words[i];
+      if (i < words.size() - 1) std::cout << ' ';
     }
 
     // closing color
-    std::cout << "\x1b[0m\n";
+    std::cout << borderStyle << "\x1b[0m\n";
   }
 };
 
-bool completedLabelCallback(mt_uti::CLIParser<std::string> &cli) {
-  StatusPrinter::printStatusLabel(cli, StatusPrinter::LABEL_COMPLETED);
-  return true;
-}
+bool messageCallback(mt_uti::CLIParser<std::string> &cli) {
 
-bool failedLabelCallback(mt_uti::CLIParser<std::string> &cli) {
-  StatusPrinter::printStatusLabel(cli, StatusPrinter::LABEL_FAILED);
+  constexpr int _not_found = -1;
+  StatusPrinter::STATUS_CODE foundStatus;
+  std::string foundKeyword;
+
+  int completedOrder = cli.getMainUnormapOrder("-completed"),
+    failedOrder = cli.getMainUnormapOrder("-failed");
+
+  // all found
+  if (failedOrder != _not_found &&
+    completedOrder != _not_found
+  ) {
+    if (completedOrder < failedOrder) {
+      foundStatus = StatusPrinter::STATUS_COMPLETED;
+      foundKeyword = "-completed";
+    }
+    else {
+      foundStatus = StatusPrinter::STATUS_FAILED;
+      foundKeyword = "-failed";
+    }
+  }
+  // completed found
+  else if (completedOrder != _not_found) {
+    foundStatus = StatusPrinter::STATUS_COMPLETED;
+    foundKeyword = "-completed";
+  }
+  // failed found
+  else if (failedOrder != _not_found) {
+    foundStatus = StatusPrinter::STATUS_FAILED;
+    foundKeyword = "-failed";
+  }
+
+  // any keyword found
+  if (!foundKeyword.empty()) {
+    mt::VEC_STR words = cli.getVectorAt<std::string>(foundKeyword);
+    std::string borderStyle = cli.getAt<std::string>("-border-style", 0);
+
+    // the keyword followed by input
+    if (!words.empty()) {
+      StatusPrinter::displayStatusMessage(
+        words, borderStyle, foundStatus
+      );
+    }
+  }
+
   return true;
 }
 
@@ -67,33 +96,27 @@ int main(int argc, char *argv[]) {
 
   /** PARSERS */
 
-  mt_uti::CLIParser<std::string> completedLabelCLI(
+  mt_uti::CLIParser<std::string> messageCLI(
     mt_uti::StrTools::argvToStringVector(argc, argv),
-    {std::make_pair("-message", "")}
-  );
-
-  mt_uti::CLIParser<std::string, mt::LD, bool> failedLabelCLI(
-    mt_uti::StrTools::argvToStringVector(argc, argv),
-    {std::make_pair("-message", "")}
+    {
+      std::make_pair("-border-style", "**"),
+      std::make_pair("-failed", ""),
+      std::make_pair("-completed", "")
+    }
   );
 
   /** WRAPPERS */
 
-  Helper::CLIWrapper::setNumberOfRuns(2);
   Helper::CLIWrapper::hideFinalMessage();
 
   Helper::CLIWrapper::run<std::string>(
-    "--completed-label",
-    "Printed when the program returns a zero value.",
-    completedLabelCLI,
-    completedLabelCallback
-  );
-
-  Helper::CLIWrapper::run<std::string, mt::LD, bool>(
-    "--failed-label",
-    "Printed when the program returns a non-zero value.",
-    failedLabelCLI,
-    failedLabelCallback
+    "--message",
+    "Print message for completed (return 0) or failed (return 1) executable.\n"
+    "Select either '-completed' or '-failed' to display a message.\n"
+    "Only the first detected keyword input will be displayed.\n"
+    "Set the left and right border style by specifying '-border-style'.",
+    messageCLI,
+    messageCallback
   );
 
   return 0;
